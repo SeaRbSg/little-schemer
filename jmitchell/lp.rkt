40ad63780d955c09292fb8632858cd3c365479a5
Jake Mitchell <jacob.d.mitchell@gmail.com>
2014-11-29 23:47:45 -0800

    Notes for chapter 3.

--- jmitchell/lp.rkt
+++ jmitchell/lp.rkt
@@ -45,6 +45,7 @@ Although it's good to know what lists and atoms are and how they differ, it's al
 
 The @tt{eq?} operation answers a yes-or-no question much like the other @tt{?}-suffixed operations, but it accepts two arguments instead of just one. It asks, "are these two non-numeric atom values equal?" A few footnotes indicate that in practice @tt{eq?} may be defined over more types than that. Some quick experiments in the Racket REPL confirm that it seems to work on numeric atoms and lists too.
 
+
 @section{Do It, Do It Again, and Again, and Again...}
 
 operations: lat? (stands for list-of-atoms?), member?
@@ -52,3 +53,107 @@ operations: lat? (stands for list-of-atoms?), member?
 keywords: define, lambda, cond, else, or
 
 literal values: #t, #f
+
+
+@section{Cons the Magnificent}
+
+@tt{rember} stands for @italic{remove member}. It takes a value @tt{a} and a list @tt{lst} and returns the same list, except with the first copy of @tt{a} removed. The @tt{cons} operation which combines anything with a list to form a pair (or equivalently, another list) makes it possible to preseve the front of the list while recursively @tt{rember}-ing the rest of the list. Once the base case of a null list is reached, the recursion unwinds and the @tt{cons}es are applied to construct the final list.
+
+@chunk[<rember>
+(define rember
+  (lambda (a lst)
+    (cond ((null? lst) '())
+          ((eq? (car lst) a) (cdr lst))
+          (else (cons (car lst) (rember a (cdr lst)))))))]
+
+@tt{firsts} takes a list of lists and returns the list consisting of the first element from each inner list. For instance, @tt{(firsts '((a b) (c d)))} evaluates to @tt{'(a c)}. Like all recursive functions on lists, the first thing to check is whether the input list is null. If it is, there is no inner list from which to extract a first element, so the only thing to do is return @tt{'()}. Otherwise, there must be a first element and since the input is supposed to be a list of lists we expect this first element to be a list. The only part of this first inner list we need is the first element, so it's extracted using @tt{caar} (same as @tt{(lambda (x) (car (car x)))}), and then @tt{cons}ed to the @tt{firsts} of the rest of the inner lists. Note that if any of the elements in the input list are @tt{'()}, @tt{caar} will eventually raise an error.
+
+@chunk[<firsts>
+(define firsts
+  (lambda (lst)
+    (cond ((null? lst) '())
+          (else (cons (caar lst)
+                      (firsts (cdr lst)))))))]
+
+@tt{seconds} is just like @tt{firsts}, except it extracts the second element from each inner list. In fact, the only difference aside from the name is the use of @tt{cadar} instead of @tt{caar}. Another similarity is unfortunate the lack of robustness: @tt{cadar} will raise an error whenever one of the inner lists has fewer than two elements. Later examples will get more sophisticated and robust.
+
+@chunk[<seconds>
+(define seconds
+  (lambda (lst)
+    (cond ((null? lst) '())
+          (else (cons (cadar lst)
+                      (seconds (cdr lst)))))))]
+
+@tt{insertR} scans a list of atoms @tt{lat} for an element equal to @tt{old}. Upon finding a match, @tt{new} is inserted to its right and the resulting list is returned.
+
+@chunk[<insertR>
+(define insertR
+  (lambda (new old lat)
+    (cond ((null? lat) '())
+          ((eq? (car lat) old) (cons old (cons new (cdr lat))))
+          (else (cons (car lat)
+                      (insertR new old (cdr lat)))))))]
+
+@tt{insertL} is the same as @tt{insertR}, except the @tt{new} value is inserted to the left of the first element that equals @tt{old}. Notice the @tt{eq?} case is much simpler because when @tt{(eq? (car lat) old)} is true, @tt{lat} starts with @tt{old} and all that remains is inserting @tt{new} to the left of it. @tt{insertR} had to extract the @tt{old} element so it could be transposed with the @tt{new} value.
+
+@chunk[<insertL>
+(define insertL
+  (lambda (new old lat)
+    (cond ((null? lat) '())
+          ((eq? (car lat) old) (cons new lat))
+          (else (cons (car lat)
+                      (insertL new old (cdr lat)))))))]
+
+@tt{subst} is the same as @tt{insertR}/@tt{insertL}, except the first element equal to @tt{old} is removed. The @tt{new} value is @italic{subst}ituted in its place.
+
+@chunk[<subst>
+(define subst
+  (lambda (new old lat)
+    (cond ((null? lat) '())
+          ((eq? (car lat) old) (cons new
+                                     (cdr lat)))
+          (else (cons (car lat)
+                      (subst new old (cdr lat)))))))]
+
+@tt{subst2} resembles @tt{subst}, but it instead takes two old values, @tt{o1} and @tt{o2}, and substitutes the first occurrence of either with @tt{new}. The implementation is almost the same, except @tt{or} is used to check whether the front of the list is equal to either @tt{o1} or @tt{o2}.
+
+@chunk[<subst2>
+(define subst2
+  (lambda (new o1 o2 lat)
+    (cond ((null? lat) '())
+          ((or (eq? (car lat) o1)
+               (eq? (car lat) o2)) (cons new (cdr lat)))
+          (else (cons (car lat)
+                      (subst2 new o1 o2 (cdr lat)))))))]
+
+For the most part functions defined in the chapter have been scanning a list until some predicate evaluates to true, at which point the recursive calls stop. For instance, @tt{(rember 'a '(x a a a))} would see that the first element, @tt{x}, is not the same as @tt{a} and recurse with @tt{(cons 'x (rember 'a '(a a a)))}, at which point the first element matches @tt{a}. Only the first match is impacted, so this example evaluates to @tt{'(x a a)}. Although these functions are useful, often the same logic should be applied to the rest of the list, even after finding the first match. @tt{multirember} does just that: it removes every element in the atom list equal to @tt{a}.
+
+@chunk[<multirember>
+(define multirember
+  (lambda (a lat)
+    (cond ((null? lat) '())
+          ((eq? (car lat) a) (multirember a (cdr lat)))
+          (else (cons (car lat) (multirember a (cdr lat)))))))]
+
+Notice the following @tt{multi}* functions resemble their non-multi analogues, except they recurse even when the predicate is true. Additionally, they all adhere to the Fourth Commandment, passing different and smaller input in every recursive call. Doing otherwise would likely result in infinite loops.
+
+@chunk[<multiinsertR>
+(define multiinsertR
+  (lambda (new old lat)
+    (cond ((null? lat) '())
+          ((eq? (car lat) old) (cons old (cons new (multiinsertR new old (cdr lat)))))
+          (else (cons (car lat) (multiinsertR new old (cdr lat)))))))]
+
+@chunk[<multiinsertL>
+(define multiinsertL
+  (lambda (new old lat)
+    (cond ((null? lat) '())
+          ((eq? (car lat) old) (cons new (cons old (multiinsertL new old (cdr lat)))))
+          (else (cons (car lat) (multiinsertL new old (cdr lat)))))))]
+
+@chunk[<multisubst>
+(define multisubst
+  (lambda (new old lat)
+    (cond ((null? lat) '())
+          ((eq? (car lat) old) (cons new (multisubst new old (cdr lat))))
+          (else (cons (car lat) (multisubst new old (cdr lat)))))))]
