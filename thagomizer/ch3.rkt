3a5953e37cda3bbdb29df2ca3d8fa10a4cf40fdf
Aja Hammerly <aja.hammerly+github@gmail.com>
2014-12-02 14:52:15 -0800

    Removed unnecessary definitions

--- thagomizer/ch3.rkt
+++ thagomizer/ch3.rkt
@@ -7,21 +7,6 @@
   (and (not (pair? x)) (not (null? x))))
 
 
-;; Previous Chapters
-(define lat? 
-  (lambda (l)
-    (cond
-      ((null? l) #t)
-      ((atom? (car l)) (lat? (cdr l)))
-      (else #f))))
-(define member?
-  (lambda (a lat)
-    (cond 
-     ((null? lat) #f)
-     (else (or (eq? (car lat) a)
-               (member? a (cdr lat)))))))
-
-
 ;; Chapter 3
 (define rember 
   (lambda (a lat)
