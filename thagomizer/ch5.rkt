af117f52dd9247635af0e2a9b1e708c7b4b5a2aa
Aja Hammerly <aja.hammerly+github@gmail.com>
2014-12-12 17:55:45 -0800

    Chapter 5

--- thagomizer/ch5.rkt
+++ thagomizer/ch5.rkt
@@ -5,6 +5,15 @@
 (define (atom? x)
   (and (not (pair? x)) (not (null? x))))
 
+(define eqan?
+  (lambda (a1 a2)
+    (cond
+     ((and (number? a1) (number? a2)) (= a1 a2))
+     ((or (number? a1) (number? a2)) #f)
+     (else (eq? a1 a2)))))
+
+
+;; Chapter Work
 
 (define rember*
   (lambda (a l)
@@ -72,4 +81,101 @@
                          '((orange) (split ((((orange ice))) (cream (orange)) sherbet)) (orange) (bread) (orange brandy))))
 
 
-(test-case "insertL* new old ")
+(define insertL*
+  (lambda (new old l)
+    (cond
+     ((null? l) '())
+     ((list? (car l)) (cons (insertL* new old (car l)) (insertL* new old (cdr l))))
+     ((eq? old (car l)) (cons new (cons old (insertL* new old (cdr l)))))
+     (else (cons (car l) (insertL* new old (cdr l)))))))
+
+(test-case "insertL* new old"
+           (check-equal? (insertL* 'a 'b '()) '())
+           (check-equal? (insertL* 'a 'b '(a b)) '(a a b))
+           (check-equal? (insertL* 'a 'b '(b c d)) '(a b c d))
+           (check-equal? (insertL* 'a 'b '((b) b c d)) '((a b) a b c d))
+           (check-equal? (insertL* 'a 'b '((b b)))'((a b a b)))
+           (check-equal? (insertL* 'p 'ch '((h m (w)) co ((a (w) ch)) (((ch))) (if (a) ((w ch))) co ch w)) '((h m (w)) co ((a (w) p ch)) (((p ch))) (if (a) ((w p ch))) co p ch w)))
+
+
+(define member* 
+  (lambda (a l)
+    (cond
+     ((null? l) #f)
+     ((list? (car l)) (or (member* a (car l)) (member* a (cdr l))))
+     ((eq? a (car l)) #t)
+     (else (member* a (cdr l))))))
+
+(test-case "member* a l"
+           (check-true (member* 'chips '((potato) (chips ((with) fish) (chisp))))))
+
+
+(define leftmost
+  (lambda (l) 
+    (cond
+     ((null? l) '())
+     ((list? (car l)) (leftmost (car l)))
+     (else (car l)))))
+
+(test-case "leftmost l"
+           (check-eq? (leftmost '()) '())
+           (check-eq? (leftmost '(a b c)) 'a)
+           (check-eq? (leftmost '(((hot) (tuna (and))) cheese)) 'hot)
+           (check-eq? (leftmost '(((() four)) 17 (seventeen))) '())
+           (check-eq? (leftmost '((potato (chips (with) fish) (chips)))) 'potato))
+
+
+
+
+(define eqlist?
+  (lambda (l1 l2)
+    (cond
+     ((and (null? l1) (null? l2)) #t)
+     ((or (null? l1) (null? l2)) #f)
+     ((and (atom? (car l1)) (atom? (car l2)))
+      (and (eqan? (car l1) (car l2)) (eqlist? (cdr l1) (cdr l2))))
+     ((or (atom? (car l1)) (atom? (car l2))) #f)
+     (else (and (eqlist? (car l1) (car l2)) (eqlist? (cdr l1) (cdr l2)))))))
+
+(test-case "eqlist?" 
+           (check-false (eqlist? '(strawberry ice cream) '(strawberry cream ice)))
+           (check-false (eqlist? '(banana ((split))) '((banana) (split))))
+           (check-false (eqlist? '(beef ((sausage)) (and (soda))) '(beef ((salami)) (and (soda)))))
+           (check-true (eqlist? '(beef ((sausage)) (and (soda))) '(beef ((sausage)) (and (soda)))))
+           (check-true (eqlist? '(strawberry ice cream) '(strawberry ice cream))))
+
+
+(define my_equal?
+  (lambda (s1 s2) 
+    (cond 
+     ((and (atom? s1) (atom? s2)) (eqan? s1 s2))
+     ((or (atom? s1) (atom? s2)) #f)
+     (else (eqlist? s1 s2)))))
+
+
+(define eqlist2?
+  (lambda (l1 l2)
+    (cond
+     ((and (null? l1) (null? l2)) #t)
+     ((or (null? l1) (null? l2)) #f)
+     (else (and (my_equal? (car l1) (car l2))
+                (my_equal? (cdr l1) (cdr l2)))))))
+
+(test-case "eqlist2?" 
+           (check-false (eqlist2? '(strawberry ice cream) '(strawberry cream ice)))
+           (check-false (eqlist2? '(banana ((split))) '((banana) (split))))
+           (check-false (eqlist2? '(beef ((sausage)) (and (soda))) '(beef ((salami)) (and (soda)))))
+           (check-true (eqlist2? '(beef ((sausage)) (and (soda))) '(beef ((sausage)) (and (soda)))))
+           (check-true (eqlist2? '(strawberry ice cream) '(strawberry ice cream))))
+
+
+(define rember
+  (lambda (s l)
+    (cond 
+     ((null? l) '())
+     ((equal? (car l) s) (cdr l))
+     (else (cons (car l) (rember s (cdr l)))))))
+
+(test-case "rember"
+           (check-equal? (rember 'a '(a b c)) '(b c))
+           (check-equal? (rember '(a b) '(z (a b) x ((y)))) '(z x ((y)))))
