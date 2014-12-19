d5c60e6bce1003b390f980863d9766c524875d17
Javier Soto <sotoseattle@gmail.com>
2014-12-19 14:44:44 -0800

    refactor and cleanup

--- sotoseattle/sandbox/99_bottles/99_bottles.rkt
+++ sotoseattle/sandbox/99_bottles/99_bottles.rkt
@@ -5,47 +5,50 @@
 
 (define countdown
   (lambda (n)
-    (cond 
+    (cond
       ((zero? n) '(0))
       (else (cons n (countdown (sub1 n)))))))
 
 (module+ test
   (check-equal? (countdown 3) '(3 2 1 0)))
 
+(define concat
+  (lambda (lat1 lat2)
+    (cond 
+      ((null? lat1) lat2)
+      (else (cons (car lat1) (concat (cdr lat1) lat2))))))
+
 ;;; segments of verses
 (define bottles_of_beer
   (lambda (n)
     (cond
-      ((eq? n 1) (cons n '(bottle of beer)))
       ((zero? n) '(no more bottles of beer))
+      ((eq? n 1) (cons n '(bottle of beer)))
       (else (cons n '(bottles of beer))))))
 
-(define bottles_on_wall
+(define on_wall
   (lambda (n)
-    (cons (bottles_of_beer n) '(on the wall))))
+    (concat (bottles_of_beer n) '(on the wall *))))
 
 ;;; verses as lines in the song, grouped in chorus
 (define verse_1
   (lambda (n)
-    (cons (bottles_on_wall n) (bottles_of_beer n))))
+    (concat (on_wall n) (bottles_of_beer n))))
 
 (define verse_2
   (lambda (n)
     (cond
       ((eq? n -1) '(Go to the store and buy some more!!!!!!))
-      (else
-        (cons '(Take one down and pass it around) (bottles_on_wall n))))))
+      (else (concat '(* Take one down and pass it around *) (on_wall n))))))
 
 (define chorus
   (lambda (n)
-    (cons (verse_1 n) (verse_2 (sub1 n)))))
+    (concat (verse_1 n) (verse_2 (sub1 n)))))
 
-;;; al together now!!
 (define sing
-  (lambda (lan)
+  (lambda (n)
     (cond
-      ((null? lan) lan)
-      (else
-        (cons (chorus(car lan)) (sing (cdr lan)))))))
+      ((eq? n -1) '())
+      (else (cons (chorus n) (sing (sub1 n)))))))
 
-(sing (countdown 3)) ;; Bravo!!
+(sing 99) ;; Bravo!!
