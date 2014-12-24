21a66a5ba148ffa44f1253f97f30c0a91dd1764b
Javier Soto <sotoseattle@gmail.com>
2014-12-24 08:47:17 -0800

    adherence to using [] for conditions

--- sotoseattle/ch7.rkt
+++ sotoseattle/ch7.rkt
@@ -6,49 +6,47 @@
 (define member? ; <--------------------- MEMBER? from chp2
   (lambda (a lat)
     (cond
-      ((null? lat) #f)
-      (else (or (eq? (car lat) a) (member? a (cdr lat)))))))
+      [(null? lat) #f]
+      [else (or (eq? (car lat) a) (member? a (cdr lat)))])))
 
 (define set?
   (lambda (lat)
     (cond
-      ((null? lat) #t)
-      ((member? (car lat) (cdr lat)) #f)
-      (else (set? (cdr lat))))))
+      [(null? lat) #t]
+      [(member? (car lat) (cdr lat)) #f]
+      [else (set? (cdr lat))])))
 
 (module+ test
-  (check-false (set? '(1 2 1 3)))
-  (check-true (set? '(1 2 3))))
+  [check-false (set? '(1 2 1 3))]
+  [check-true (set? '(1 2 3))])
 
 (define makeset
   (lambda (lat)
     (cond
-      ((null? lat) lat)
-      ((member? (car lat) (cdr lat)) (makeset (cdr lat)))
-      (else (cons (car lat) (makeset (cdr lat)))))))
+      [(null? lat) lat]
+      [(member? (car lat) (cdr lat)) (makeset (cdr lat))]
+      [else (cons (car lat) (makeset (cdr lat)))])))
 
 (module+ test
-  (check-true (set? (makeset '(1 2 1 3 1 3 3))))
-  (check-true (set? (makeset '(1 2 3)))))
+  [check-true (set? (makeset '(1 2 1 3 1 3 3)))]
+  [check-true (set? (makeset '(1 2 3)))])
 
 (define multirember ; <--------------------- MULTIREMBER from chp3
   (lambda (a lat)
     (cond
-      ((null? lat) lat)
-      (else
-       (cond
-         ((eq? a (car lat)) (multirember a (cdr lat)))
-         (else (cons (car lat) (multirember a (cdr lat)))))))))
+      [(null? lat) lat]
+         [(eq? a (car lat)) (multirember a (cdr lat))]
+         [else (cons (car lat) (multirember a (cdr lat)))])))
 
 (define makeset_v2
   (lambda (lat)
     (cond
-      ((null? lat) lat)
-      (else (cons (car lat) (multirember (car lat) (makeset_v2 (cdr lat)))))))) ; my version
-;     (else (cons (car lat) (makeset2 (multirember (car lat) (cdr lat)))))))) ; book version
+      [(null? lat) lat]
+      [else (cons (car lat) (multirember (car lat) (makeset_v2 (cdr lat))))]))) ; my version
+;     [else (cons (car lat) (makeset2 (multirember (car lat) (cdr lat))))]))) ; book version
 
-;;;;;;;;;;;;; QUESTION 1: I have seen that many times the above happens. There is more than one way to
-;;;;;;;;;;;;; to structure the recursive logic. Test pass for both cases. Am I missing something?
+;;;;;;;;;;;;; NOTE: I have seen that many times the above happens. There is more than one way to
+;;;;;;;;;;;;; to structure the recursive logic. Test pass for both cases.
 ;;;;;;;;;;;;; a first difference is that, when using cons, the logic structure determines the order
 ;;;;;;;;;;;;; of stuff showing up in the list (beware)
 ;;;;;;;;;;;;; UPDATE: As far as I have seen this symetry is a mirage, sometimes happens sometimes it doesnt
@@ -57,14 +55,14 @@
 (define countdown
   (lambda (n)
     (cond
-      ((zero? n) '())
-      (else (cons n (countdown (sub1 n)))))))
+      [(zero? n) '()]
+      [else (cons n (countdown (sub1 n)))])))
 
 (define countup
   (lambda (n acc)
     (cond
-      ((zero? n) acc)
-      (else (countup (sub1 n) (cons n acc))))))
+      [(zero? n) acc]
+      [else (countup (sub1 n) (cons n acc))])))
 
 (module+ test
   (check-equal? (countdown 3)   '(3 2 1))
@@ -74,81 +72,80 @@
 ;;;;;;;;;;;;;
 
 (module+ test
-  (check-true (set? (makeset_v2 '(1 2 1 3 1 3 3))))
-  (check-true (set? (makeset_v2 '(1 2 3))))
+  [check-true (set? (makeset_v2 '(1 2 1 3 1 3 3)))]
+  [check-true (set? (makeset_v2 '(1 2 3)))]
   (check-equal? (makeset_v2 '(apple peach pear peach plum apple lemon peach)) '(apple peach pear plum lemon))
   (check-equal? (makeset_v2 '(apple 3 pear 4 9 apple 3 4)) '(apple 3 pear 4 9)))
 
 (define subset?
   (lambda (set1 set2)
     (cond
-      ((null? set1) #t)
-      ((member? (car set1) set2) (subset? (cdr set1) set2))
-      (else #f))))
+      [(null? set1) #t]
+      [(member? (car set1) set2) (subset? (cdr set1) set2)]
+      [else #f])))
 
 (module+ test
-  (check-true (subset? '(5 chicken wings) '(5 hamburgers 2 piece fried chicken and light duckling wings)))
-  (check-false (subset? '(4 pounds horseradish) '(four pounds chicken and 5 ounces horseradish))))
+  [check-true (subset? '(5 chicken wings) '(5 hamburgers 2 piece fried chicken and light duckling wings))]
+  [check-false (subset? '(4 pounds horseradish) '(four pounds chicken and 5 ounces horseradish))])
 
 (define subset_v2? ; instead of return the answer to a question, take advantage of it being a question and use (and
   (lambda (set1 set2)
     (cond
-      ((null? set1) #t)
-      (else (and (member? (car set1) set2) (subset_v2? (cdr set1) set2))))))
+      [(null? set1) #t]
+      [else (and (member? (car set1) set2) (subset_v2? (cdr set1) set2))])))
 
 (module+ test
-  (check-true (subset_v2? '(5 chicken wings) '(5 hamburgers 2 piece fried chicken and light duckling wings)))
-  (check-false (subset_v2? '(4 pounds horseradish) '(four pounds chicken and 5 ounces horseradish))))
+  [check-true (subset_v2? '(5 chicken wings) '(5 hamburgers 2 piece fried chicken and light duckling wings))]
+  [check-false (subset_v2? '(4 pounds horseradish) '(four pounds chicken and 5 ounces horseradish))])
 
 (define eqset_soto? ; my aproach before looking the solution (there was no need to do all that)
   (lambda (set1 set2)
     (cond
-      ((and (null? set1) (null? set2)) #t)
-      ((member? (car set1) set2) (eqset_soto? (cdr set1) (multirember (car set1) set2)))
-      (else #f))))
+      [(and (null? set1) (null? set2)) #t]
+      [(member? (car set1) set2) (eqset_soto? (cdr set1) (multirember (car set1) set2))]
+      [else #f])))
 
 (module+ test
-  (check-true (eqset_soto? '(6 large chicken with wings) '(6 large chicken with wings))))
+  [check-true (eqset_soto? '(6 large chicken with wings) '(6 large chicken with wings))])
 
 (define eqset?
   (lambda (set1 set2)
-    (and (subset? set1 set2) (subset? set2 set1)))) ; niiiiiiceeeeee
+    [and (subset? set1 set2) (subset? set2 set1)])) ; niiiiiiceeeeee
 
 (module+ test
-  (check-true (eqset? '(6 large chicken with wings) '(6 large chicken with wings))))
+  [check-true (eqset? '(6 large chicken with wings) '(6 large chicken with wings))])
 
 (define intersect?
   (lambda (set1 set2)
     (cond
-      ((null? set1) #f)
-      ((member? (car set1) set2) #t)
-      (else (intersect? (cdr set1) set2)))))
+      [(null? set1) #f]
+      [(member? (car set1) set2) #t]
+      [else (intersect? (cdr set1) set2)])))
 
 (module+ test
-  (check-true  (intersect? '(1 2 3 4 5 6) '(6 7 8 9)))
-  (check-false (intersect? '(1 2 3 4 5) '(6 7 8 9)))
-  (check-false (intersect? '(1 2 3 4 5 6) '()))
-  (check-false (intersect? '() '())))
+  [check-true  (intersect? '(1 2 3 4 5 6) '(6 7 8 9))]
+  [check-false (intersect? '(1 2 3 4 5) '(6 7 8 9))]
+  [check-false (intersect? '(1 2 3 4 5 6) '())]
+  [check-false (intersect? '() '())])
 
 (define intersect_v2? ; intersect with 'or'
   (lambda (set1 set2)
     (cond
-      ((null? set1) #f)
-      (else (or  (member? (car set1) set2) (intersect_v2? (cdr set1) set2))))))
-;     (else (and (member? (car set1) set2) (subset_v2?    (cdr set1) set2)))))) ; comparison with (subset?
+      [(null? set1) #f]
+      [else (or  (member? (car set1) set2) (intersect_v2? (cdr set1) set2))])))
 
 (module+ test
-  (check-true  (intersect_v2? '(1 2 3 4 5 6) '(6 7 8 9)))
-  (check-false (intersect_v2? '(1 2 3 4 5) '(6 7 8 9)))
-  (check-false (intersect_v2? '(1 2 3 4 5 6) '()))
-  (check-false (intersect_v2? '() '())))
+  [check-true  (intersect_v2? '(1 2 3 4 5 6) '(6 7 8 9))]
+  [check-false (intersect_v2? '(1 2 3 4 5) '(6 7 8 9))]
+  [check-false (intersect_v2? '(1 2 3 4 5 6) '())]
+  [check-false (intersect_v2? '() '())])
 
 (define intersect
   (lambda (set1 set2)
     (cond
-      ((null? set1) set1)
-      ((member? (car set1) set2) (cons (car set1) (intersect (cdr set1) set2)))
-      (else (intersect (cdr set1) set2)))))
+      [(null? set1) set1]
+      [(member? (car set1) set2) (cons (car set1) (intersect (cdr set1) set2))]
+      [else (intersect (cdr set1) set2)])))
 
 (module+ test
   (check-equal? (intersect '(1 2 3 4 5 6) '(6 7 8 9)) '(6)))
@@ -156,10 +153,10 @@
 (define union
   (lambda (set1 set2)
     (cond
-      ((null? set1) set2)
-      ((member? (car set1) set2) (union (cdr set1) set2))
-      (else (cons (car set1) (union (cdr set1) set2))))))
-;     (else (union (cdr set1) (cons (car set1) set2)))))) ; my initial approach (I blame the dislexya)
+      [(null? set1) set2]
+      [(member? (car set1) set2) (union (cdr set1) set2)]
+      [else (cons (car set1) (union (cdr set1) set2))])))
+;     [else (union (cdr set1) (cons (car set1) set2))]))) ; my initial approach (I blame the dislexya)
                                                           ; the logic determines the order of the atoms in the list
 
 (module+ test
@@ -169,8 +166,8 @@
 (define union_soto ; my version: just cons both sets and then call (makeset !!
   (lambda (set1 set2)
     (cond
-      ((null? set1) set2)
-      (else (makeset (cons (car set1) (union_soto (cdr set1) set2)))))))
+      [(null? set1) set2]
+      [else (makeset (cons (car set1) (union_soto (cdr set1) set2)))])))
 
 (module+ test
   (check-equal? (union_soto '(1 2 3 4 5 6) '(6 7 8 9)) '(1 2 3 4 5 6 7 8 9))
@@ -179,9 +176,9 @@
 (define xxx
   (lambda (set1 set2)
     (cond
-      ((null? set1) set1)
-      ((member? (car set1) set2) (xxx (cdr set1) set2))
-      (else (cons (car set1) (xxx (cdr set1) set2))))))
+      [(null? set1) set1]
+      [(member? (car set1) set2) (xxx (cdr set1) set2)]
+      [else (cons (car set1) (xxx (cdr set1) set2))])))
 
 (module+ test
   (check-equal? (xxx '(1 2 7 9 0) '(6 7 8 9)) '(1 2 0)))
@@ -189,8 +186,8 @@
 (define intersectall
   (lambda (lst)
     (cond
-      ((null? (cdr lst)) (car lst))
-      (else (intersect (car lst) (intersectall (cdr lst)))))))
+      [(null? (cdr lst)) (car lst)]
+      [else (intersect (car lst) (intersectall (cdr lst)))])))
 
 (module+ test
   (check-equal? (intersectall '((a b c) (c a d e) (e f g h a b))) '(a)))
@@ -198,14 +195,14 @@
 (define a-pair?
   (lambda (x)
     (cond
-      ((or (atom? x) (or (null? x) (null? (cdr x)))) #f)
-      ((null? (cdr (cdr x))) #t)
-      (else #f))))
+      [(or (atom? x) (or (null? x) (null? (cdr x)))) #f]
+      [(null? (cdr (cdr x))) #t]
+      [else #f])))
 
 (module+ test
-  (check-true (a-pair? '(1 2)))
-  (check-true (a-pair? '((1) pepe)))
-  (check-true (a-pair? '(juan (casimiro)))))
+  [check-true (a-pair? '(1 2))]
+  [check-true (a-pair? '((1) pepe))]
+  [check-true (a-pair? '(juan (casimiro)))])
 
 (define first
   (lambda (p)
@@ -229,8 +226,8 @@
 (define firsts
   (lambda (rel)
     (cond
-      ((null? rel) rel)
-      (else (cons (first (car rel)) (firsts (cdr rel)))))))
+      [(null? rel) rel]
+      [else (cons (first (car rel)) (firsts (cdr rel)))])))
 
 (module+ test
   (check-equal? (firsts '((1 2) (3 4) (5 6))) '(1 3 5)))
@@ -242,8 +239,8 @@
 (define revrel_v1
   (lambda (rel)
     (cond
-      ((null? rel) rel)
-      (else (cons (build (second (car rel)) (first (car rel))) (revrel_v1 (cdr rel)))))))
+      [(null? rel) rel]
+      [else (cons (build (second (car rel)) (first (car rel))) (revrel_v1 (cdr rel)))])))
 
 (module+ test
   (check-equal? (revrel_v1 '((1 2) (3 4))) '((2 1) (4 3))))
@@ -255,8 +252,8 @@
 (define revrel
   (lambda (rel)
     (cond
-      ((null? rel) rel)
-      (else (cons (revpair (car rel)) (revrel (cdr rel)))))))
+      [(null? rel) rel]
+      [else (cons (revpair (car rel)) (revrel (cdr rel)))])))
 
 (module+ test
   (check-equal? (revrel '((1 2) (3 4))) '((2 1) (4 3))))
@@ -264,8 +261,8 @@
 (define seconds
   (lambda (rel)
     (cond
-      ((null? rel) rel)
-      (else (cons (second (car rel)) (seconds (cdr rel)))))))
+      [(null? rel) rel]
+      [else (cons (second (car rel)) (seconds (cdr rel)))])))
 
 (define fullfun?                                  ; assumes it is already fun
   (lambda (fun)
@@ -279,23 +276,22 @@
   (lambda (rel)                                   ; set of firsts and set of seconds,
     (and (fun? rel) (fun? (revrel rel)))))        ; which means is a set in three ways, so set for life.
 
-;;;;;;;;;;;;; QUESTION 2: OMG Lito is right. A fun is a hash !!!
-;;;;;;;;;;;;; This surely deserves some talking next Tuesday
-;;;;;;;;;;;;; Here is my atempt at making fun
+;;;;;;;;;;;;; NOTE 2: Although a fun can be seen as a hash, it should be better seen as a table,
+;;;;;;;;;;;;; not a dictionary (for which scheme has assoc). Here is my atempt at making fun a hash
 
 (define update_fun
   (lambda (key fun)
     (cond
-      ((null? fun) fun)
-      ((eq? key (first (car fun))) (cons (build key (add1 (second (car fun)))) (update_fun key (cdr fun))))
-      (else (cons (car fun) (update_fun key (cdr fun)))))))
+      [(null? fun) fun]
+      [(eq? key (first (car fun))) (cons (build key (add1 (second (car fun)))) (update_fun key (cdr fun)))]
+      [else (cons (car fun) (update_fun key (cdr fun)))])))
 
 (define wordcount
   (lambda (lat fun)
     (cond
-      ((null? lat) (reverse fun))                                                         ; for readability
-      ((member? (car lat) (firsts fun)) (wordcount (cdr lat) (update_fun (car lat) fun))) ; if car is key in fun add 1
-      (else (wordcount (cdr lat) (cons (build (car lat) 1) fun))))))                      ; else make new pair with  1
+      [(null? lat) (reverse fun)]                                                         ; for readability
+      [(member? (car lat) (firsts fun)) (wordcount (cdr lat) (update_fun (car lat) fun))] ; if car is key in fun add 1
+      [else (wordcount (cdr lat) (cons (build (car lat) 1) fun))])))                      ; else make new pair with  1
 
 (module+ test
   (check-equal? (update_fun 'b '((a 3) (b 4) (c 5))) '((a 3) (b 5) (c 5)))
