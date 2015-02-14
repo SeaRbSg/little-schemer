ffb97e0cb99c21ef3c7a8333cbcb055507df4ce4
Scott Windsor <swindsor@gmail.com>
2015-02-14 13:52:01 -0800

    Chapter 12 complete

--- sentientmonkey/ch12.rkt
+++ sentientmonkey/ch12.rkt
@@ -3,7 +3,7 @@
 (require rackunit)
 (require "prelude.rkt")
 (require "ch03.rkt")
-(require "ch07.rkt")
+(require "ch04.rkt")
 (require "ch08.rkt")
 (require "ch09.rkt")
 
@@ -54,5 +54,64 @@
 
 (check-false (member-let? 'ice '(salad greens with pears brie cheese frozen yogurt)))
 
+(define (union set1 set2)
+  (letrec
+    ([U (lambda (s)
+          (cond
+            [(null? s) set2]
+            [(M? (car s) set2) set2]
+            [else (cons (car s) (U (cdr s)))]))]
+     [M? (lambda (a lat)
+           (letrec
+             ([N? (lambda (lat)
+                    (cond
+                      [(null? lat) #f]
+                      [(eq? (car lat) a) #t]
+                      [else (N? (cdr lat))]))])
+             (N? lat)))])
+    (U set1)))
 
 (check-equal? (union '(tomatoes and macaroni cheese) '(macaroni and cheese)) '(tomatoes macaroni and cheese))
+
+(define two-in-a-row?
+  (letrec
+    ([T? (lambda (a lat)
+           (cond [(null? lat) #f]
+                 [else (or (eq? (car lat) a)
+                           (T? (car lat) (cdr lat)))]))])
+    (lambda (lat)
+      (cond
+        [(null? lat) #f]
+        [else (T? (car lat) (cdr lat))]))))
+
+(check-equal? (two-in-a-row? '(Italian sardines spaghetti parsley)) #f)
+(check-equal? (two-in-a-row? '(Italian sardines sardines spaghetti parsley)) #t)
+
+(define (sum-of-prefixes tup)
+  (letrec
+    ([S (lambda (ssof tup)
+          (cond [(null? tup) '()]
+                [else (cons (+ ssof (car tup))
+                            (S (+ ssof (car tup)) (cdr tup)))]))])
+    (S 0 tup)))
+
+; I think I prefer the sum-of-prefixes style over the lambda defined inside the
+; letrec. Makes it much easier to see arguments for the define using the condensed
+; racket style.
+
+(check-equal? (sum-of-prefixes '(2 1 9 17 0)) '(2 3 12 29 29))
+(check-equal? (sum-of-prefixes '(1 1 1 1 1)) '(1 2 3 4 5))
+
+(define (scramble tup)
+  (letrec
+    ([P (lambda (tup rev-pre)
+          (cond
+            [(null? tup) '()]
+            [else (cons (pick (car tup)
+                              (cons (car tup) rev-pre))
+                        (P (cdr tup) (cons (car tup) rev-pre)))]))])
+    (P tup '())))
+
+(check-equal? (scramble '(1 1 1 3 4 2 1 1 9 2)) '(1 1 1 1 1 4 1 1 1 9))
+(check-equal? (scramble '(1 2 3 4 5 6 7 8 9)) '(1 1 1 1 1 1 1 1 1))
+(check-equal? (scramble '(1 2 3 1 2 3 4 1 8 2 10)) '(1 1 1 1 1 1 1 1 2 8 2))
