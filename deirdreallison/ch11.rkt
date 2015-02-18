b852405573abaf50425ef37cbd07ff1fe5bd939d
DEIRDRE ALLISON <DEIRDRE9@GMAIL.com>
2015-02-17 17:09:48 -0800

    multirember no letrec

--- deirdreallison/ch11.rkt
+++ deirdreallison/ch11.rkt
@@ -3,3 +3,65 @@
 (define atom?
   (lambda (x)
     (and (not (pair? x)) (not(null? x)))))
+
+(define is-first?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      (else (eq? (car lat) a)))))
+
+(define is-first-b?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      (else (or (eq? (car lat) a)
+                (two-in-a-row? lat))))))
+
+(define two-in-a-row?
+  (lambda (lat)
+    (cond
+      ((null? lat) #f )
+      (else
+       (or (two-in-a-row-b? (car lat) 
+                            (cdr lat)))))))
+
+(define two-in-a-row-b?
+  (lambda (preceding lat)
+    (cond
+      ((null? lat) #f)
+      (else (or (eq? (car lat) preceding)
+                (two-in-a-row-b? (car lat)
+                                 (cdr lat)))))))
+
+(define sum-of-prefixes
+  (lambda (tup)
+    (sum-of-prefixes-b 0 tup)))
+
+(define sum-of-prefixes-b
+  (lambda (sonssf tup)
+    (cond
+      ((null? tup) (quote ()))
+      (else (cons (+ sonssf (car tup))
+                  (sum-of-prefixes-b
+                   (+ sonssf (car tup))
+                   (cdr tup)))))))
+
+(define pick
+  (lambda (n lat)
+    (cond
+      [(eq? n '1) (car lat)]
+      [else (pick (- n 1) (cdr lat))])))
+
+(define scramble-b
+  (lambda (tup rev-pre)
+    (cond
+      ((null? tup) (quote ()))
+      (else
+       (cons (pick (car tup)
+                   (cons (car tup) rev-pre))
+             (scramble-b (cdr tup)
+                        (cons (car tup) rev-pre)))))))
+
+(define scramble
+  (lambda (tup)
+    (scramble-b tup (quote ()))))
