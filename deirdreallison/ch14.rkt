a624b57d09c5766ad33a518cb0eb585001ffcd30
DEIRDRE ALLISON <DEIRDRE9@GMAIL.com>
2015-03-02 15:49:09 -0800

    leftmost with let, returning syntax error

--- deirdreallison/ch14.rkt
+++ deirdreallison/ch14.rkt
@@ -10,21 +10,17 @@
      (let/cc success 
        (let/cc var (success a)) . b))))
 
-(define pick
-  (lambda (n lat)
-    (cond
-      [(eq? n '1) (car lat)]
-      [else (pick (- n 1) (cdr lat))])))
-
 (define leftmost
   (lambda (l)
     (cond
       ((null? l) (quote ()))
       ((atom? (car l)) (car l))
-      (else (cond
-              ((atom? (leftmost (car l)))
-               (leftmost (car l)))
-              (else (leftmost (cdr l))))))))
+      (else 
+       (let 
+           ((a leftmost (car l))))
+       (cond
+         ((atom? a) a)
+         (else (leftmost (cdr l))))))))
 
 
 
