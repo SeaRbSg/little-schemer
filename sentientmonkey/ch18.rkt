57a105ed5384cf5046438585f80cf98a737d2d3a
Scott Windsor <swindsor@gmail.com>
2015-03-29 16:28:50 -0700

    Okay, I think I've got Ch 18 figured out now.

--- sentientmonkey/ch18.rkt
+++ sentientmonkey/ch18.rkt
@@ -4,12 +4,10 @@
 (require "prelude.rkt")
 
 (define counter 0)
-(define set-counter 0)
 
 (define consC
   (let ([N 0])
     (set! counter (lambda () N))
-    (set! set-counter (lambda (n) (set! N n)))
     (lambda (a b)
       (set! N (add1 N))
       (cons a b))))
@@ -74,30 +72,29 @@
 (define (kdr c)
   (c (lambda (s a d) d)))
 
-(define set-kdr
-  (lambda (c x)
-    ((c (lambda (s a d) s)) x)))
+(define (set-kdr c x)
+  ((c (lambda (s a d) s)) x))
 
 (define (bons kar)
   (let ([kdr '()])
     (lambda (selector)
-      (selector
-        (lambda (x) (set! kdr x))
-        kar
-        kdr))))
+      (selector (lambda (x) (set! kdr x)) kar kdr))))
 
 (define (kons a d)
   (let ([c (bons a)])
     (set-kdr c d)
     c))
 
+(test-case "kons"
+  (define breakfast (kons 'eggs '(and bacon)))
+  (check-equal? (kar breakfast) 'eggs)
+  (check-equal? (kdr breakfast) '(and bacon)))
+
 (define kounter 0)
-(define set-kounter 0)
 
 (define konsC
   (let ([N 0])
     (set! kounter (lambda () N))
-    (set! set-kounter (lambda (n) (set! N n)))
     (lambda (a b)
       (set! N (add1 N))
       (kons a b))))
@@ -107,6 +104,10 @@
     '()
     (konsC 'egg (klots (sub1 m)))))
 
+(test-case "klots"
+  (define dozen (klots 12))
+  (check-equal? (kounter) 12))
+
 (define (eklist? ls1 ls2)
   (cond [(null? ls1) (null? ls2)]
         [(null? ls2) #f]
@@ -114,21 +115,22 @@
           (and (eq? (kar ls1) (kar ls2))
                (eklist? (kdr ls1) (kdr ls2)))]))
 
-(test-case "kons & klots"
-  (define dozen (klots 12))
-  (check-equal? (kounter) 12))
+(test-case "eklist"
+  (check-true (eklist? (klots 1) (klots 1)))
+  (check-false (eklist? (klots 1) (klots 2)))
+  (check-false (eklist? (klots 1) null)))
 
 (define (kadd-at-end l)
   (if (null? l)
     (konsC 'egg '())
-    (konsC (kar l) (add-at-end (kdr l)))))
+    (konsC (kar l) (kadd-at-end (kdr l)))))
 
 (define (same? c1 c2)
   (let ([t1 (kdr c1)]
         [t2 (kdr c2)])
     (set-kdr c1 1)
     (set-kdr c2 2)
-    (let ([v (eq? (kdr c1) (kdr c2))]) ; have to use eq? instead of =
+    (let ([v (= (kdr c1) (kdr c2))])
       (set-kdr c1 t1)
       (set-kdr c2 t2)
       v)))
@@ -156,9 +158,9 @@
   (define dozen (klots 12))
   (define bakers-dozen (kadd-at-end dozen))
   (define bakers-dozen-too (kadd-at-end dozen))
-  (check-true (same? bakers-dozen bakers-dozen-too))
-  (check-true (same? (kons 'egg '())
-                     (kons 'egg '())))) ; but my same was true?
+  (check-false (same? bakers-dozen bakers-dozen-too))
+  (check-false (same? (kons 'egg '())
+                      (kons 'egg '()))))
 
 (define (last-kons ls)
   (if (null? (kdr ls))
@@ -173,8 +175,40 @@
 (define long (klots 12))
 
 (test-case "long"
-  (check-equal? (klenkth long) 12)
-  (check-equal? (set-kdr (last-kons long) long) long)
-  (check-equal? (klenkth long) 1) ; I got one. Hmm...
-  (set-kdr (last-kons long) (kdr (kdr long)))) ; fails :(
+  (check-equal? (klenkth long) 12))
 
+(define (finite-lenkth p)
+  (let/cc infinite
+    (letrec
+      ([C (lambda (p q)
+            (cond [(same? p q) (infinite #f)]
+                  [(null? q) 0]
+                  [(null? (kdr q)) 1]
+                  [else (+ (C (sl p) (qk q)) 2)]))]
+       [qk (lambda (x) (kdr (kdr x)))]
+       [sl (lambda (x) (kdr x))])
+      (cond [(null? p) 0]
+            [else (add1 (C p (kdr p)))]))))
+
+(test-case "finite-lenkth"
+  (check-equal? (finite-lenkth long) 12)
+  (set-kdr (last-kons long) long)
+  (check-equal? (finite-lenkth long) #f)
+  (check-equal? (finite-lenkth null) 0))
+
+(define mongo
+  (kons 'pie
+    (kons 'à
+      (kons 'la
+        (kons 'mode '())))))
+
+(define (klist->list l)
+  (if (null? l)
+    '()
+    (cons (kar l) (klist->list (kdr l)))))
+
+(check-equal? (klist->list mongo) '(pie à la mode))
+
+(set-kdr (kdr (kdr (kdr mongo))) (kdr mongo))
+
+(check-equal? (finite-lenkth mongo) #f) ; forever ice cream
