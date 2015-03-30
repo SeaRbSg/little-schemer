ee5d40e49290ff62168247889be92d515757cbc5
Javier Soto <sotoseattle@gmail.com>
2015-03-30 12:17:30 -0700

    ch16 reviewed from scratch

--- sotoseattle/ch16.rkt
+++ sotoseattle/ch16.rkt
@@ -1,8 +1,8 @@
 #lang racket
 (require "lib/shared.rkt")
 (require rackunit)
-;(require test-engine/racket-tests)
-;(require racket/trace)
+
+;;; page 107
 
 (define sweet-tooth
   (lambda (food)
@@ -10,233 +10,287 @@
 
 (define last 'angelfood)
 
-(module+ test
-  (define x last) ; setting x to whatever was in last for comparisson purposes
-  (check-equal? (sweet-tooth 'chocolate) '(chocolate cake))
-  (check-equal? last x)
-  (check-equal? (sweet-tooth 'fruit) '(fruit cake)))
+(test-case "page 107_a" ; Thanks Scott for this great way to test!!
+  [check-equal? (sweet-tooth 'chocolate) '(chocolate cake)]
+  [check-equal? (sweet-tooth 'fruit) '(fruit cake)]
+  [check-equal? last 'angelfood])
 
-(define sweet-tooth-L
+(define sweet-toothL
   (lambda (food)
     (set! last food)
     (cons food (cons 'cake '()))))
 
-(module+ test
-  (check-equal? (sweet-tooth-L 'chocolate) '(chocolate cake))
-  (check-equal? last 'chocolate)
-  (check-equal? (sweet-tooth-L 'fruit) '(fruit cake))
-  (check-equal? last 'fruit))
+(test-case "page 107_b"
+  [check-equal? (sweet-toothL 'chocolate) '(chocolate cake)]
+  [check-equal? last 'chocolate])
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; page 108  
+
+(test-case "page 108"
+  [check-equal? (sweet-toothL 'fruit) '(fruit cake)]
+  [check-equal? last 'fruit]
+  [check-equal? (sweet-toothL 'cheese) '(cheese cake)]
+  [check-equal? last 'cheese]
+  [check-equal? (sweet-toothL 'carrot) '(carrot cake)]
+  [check-equal? last 'carrot])
 
 (define ingredients '())
 
-(define sweet-tooth-R
+;;; page 109
+
+(define sweet-toothR
   (lambda (food)
-    (set! last food)
-    (set! ingredients (cons last ingredients))
+    (set! ingredients (cons food ingredients))
     (cons food (cons 'cake '()))))
 
-(module+ test
-  (check-equal? (sweet-tooth-R 'chocolate) '(chocolate cake))
-  (check-equal? last 'chocolate)
-  (check-equal? (sweet-tooth-R 'stinky_tofu) '(stinky_tofu cake))
-  (check-equal? last 'stinky_tofu)
-  (check-equal? (sweet-tooth-R 'garlic) '(garlic cake))
-  (check-equal? last 'garlic)
-  (check-equal? ingredients '(garlic stinky_tofu chocolate)))
+(test-case "page 109"
+  [check-equal? ingredients '()]
+  [check-equal? (sweet-toothR 'chocolate) '(chocolate cake)]
+  [check-equal? ingredients '(chocolate)]
+  [check-equal? (sweet-toothR 'fruit) '(fruit cake)]
+  [check-equal? ingredients '(fruit chocolate)]
+  [check-equal? (sweet-toothR 'cheese) '(cheese cake)]
+  [check-equal? ingredients '(cheese fruit chocolate)]
+  [check-equal? (sweet-toothR 'carrot) '(carrot cake)]
+  [check-equal? ingredients '(carrot cheese fruit chocolate)])
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; page 110 -- and yes, we have broken the outmost holy 16th commandment, Moses would be pissed!
 
 (define deep
-  (lambda (n)
-    (cond 
-      [(zero? n) 'pizza]
-      [else (cons (deep (- n 1)) '())])))
+  (lambda (m)
+    (cond
+      [(zero? m) 'pizza]
+      [else (cons (deep (sub1 m)) '())])))
 
-(module+ test
-  (check-equal? (deep 3) '(((pizza))))
-  (check-equal? (deep 0) 'pizza))
+(test-case "page 110"
+  [check-equal? (deep 3) '(((pizza)))]
+  [check-equal? (deep 7) '(((((((pizza)))))))]
+  [check-equal? (deep 0) 'pizza])
 
-(define Ns_1 '())
-
-(define deep-R  ; remember all the input arguments n
-  (lambda (n)
-    (set! Ns_1 (cons n Ns_1))
-    (deep n)))
-
-(module+ test
-  (check-equal? (deep-R 3) '(((pizza))))
-  (check-equal? Ns_1 '(3)))
+;;; page 111
 
 (define Ns '())
 (define Rs '())
 
-(define deep-R2
+;(define deepR
+;  (lambda (n)
+;    (set! Ns (cons n Ns))
+;    (set! Rs (cons (deep n) Rs))
+;    (deep n)))
+
+(define deepR  ; refactored so we compute (deep n) only once
   (lambda (n)
-    (let ((output (deep n)))
-    (set! Ns (cons n Ns))
-    (set! Rs (cons output Rs))
-    output)))
+    (let ((result (deep n)))
+      (set! Ns (cons n Ns))
+      (set! Rs (cons result Rs))
+      result)))
 
-(module+ test
-  (check-equal? (deep-R2 0) 'pizza)
-  (check-equal? (deep-R2 2) '((pizza)))
-  (check-equal? Ns '(2 0))
-  (check-equal? Rs '(((pizza)) pizza)))
+(test-case "page 111"
+  [check-equal? Ns '()]
+  [check-equal? Rs '()]
+  [check-equal? (deepR 3) '(((pizza)))]
+  [check-equal? (deepR 7) '(((((((pizza)))))))]
+  [check-equal? (deepR 0) 'pizza]
+  [check-equal? Ns '(0 7 3)]
+  [check-equal? Rs '(pizza (((((((pizza))))))) (((pizza))))])
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; page 112
 
-(define find-v1
+(test-case "page 112"
+    (set! Ns '())
+    (set! Rs '())
+    [check-equal? (deepR 3) '(((pizza)))]
+    [check-equal? (deepR 5) '(((((pizza)))))]
+    [check-equal? (deepR 3) '(((pizza)))]
+    [check-equal? Ns '(3 5 3)]
+    [check-equal? Rs '((((pizza))) (((((pizza))))) (((pizza))))])
+
+;;; page 113
+
+;(define find
+;  (lambda (n Ns Rs)
+;    (cond
+;      [(null? Ns) #f]
+;      [(eq? n (car Ns)) (car Rs)]
+;      [else (find n (cdr Ns) (cdr Rs))])))
+
+(define find  ; refactor with letrecc to abide by 12th commandment
   (lambda (n Ns Rs)
-    (cond
-      [(null? Ns) '()]
-      [(eq? n (car Ns)) (car Rs)]
-      [else (find-v1 n (cdr Ns) (cdr Rs))])))
-
-(define find-v2
-  (lambda (n Ns Rs)
     (letrec
-        ((A (lambda (ns rs)
-              (cond
-                [(eq? n (car ns)) (car rs)]
-                [else (A (cdr ns) (cdr rs))]))))
-        (A Ns Rs))))
+     ((A (lambda (enes erres)
+         (cond
+           [(null? enes) #f]
+           [(eq? n (car enes)) (car erres)]
+           [else (A (cdr enes) (cdr erres))]))))
+     (A Ns Rs))))
 
+(test-case "page 113_a"
+  [check-equal? (find 3 '(3 5 3) '((((pizza))) (((((pizza))))) (((pizza))))) '(((pizza)))]
+  [check-equal? (find 5 '(3 5 3) '((((pizza))) (((((pizza))))) (((pizza))))) '(((((pizza)))))]
+  [check-false  (find 9 '(3 5 3) '((((pizza))) (((((pizza))))) (((pizza)))))]
+)
 
-(module+ test
-  (check-equal? (find-v2 0 '(0 1) '(pepe jaime)) 'pepe)
-  (check-equal? (find-v2 2 Ns Rs) '((pizza))))
+;(define deepM
+;  (lambda (n)
+;    (let ((gotit (find n Ns Rs)))
+;      (cond
+;        (gotit gotit)
+;        [else 
+;         (let ((result (deep n)))
+;           (set! Ns (cons n Ns))
+;           (set! Rs (cons result Rs))
+;           result)]))))
 
-(define member?
-  (lambda (a lat)
-    (letrec
-        ((?? (lambda (lat)
-              (cond
-                [(null? lat) #f]
-                [(eq? a (car lat)) #t]
-                [else (?? (cdr lat))]))))
-        (?? lat))))
-
-(define deep-M
+(define deepM  ; simplified although later it goes back to that version
   (lambda (n)
-    (if (member? n Ns) (find-v2 n Ns Rs) (deep-R2 n))))
+    (let ((gotit (find n Ns Rs)))
+      (if gotit gotit (deepR n)))))
 
-(define deep-M2
+(test-case "page 113_b"
+    (set! Ns '(3 5 3))
+    (set! Rs '((((pizza))) (((((pizza))))) (((pizza)))))
+    [check-equal? (deepM 5) '(((((pizza)))))]
+    [check-equal? (deepM 3) '(((pizza)))]
+    [check-equal? Ns '(3 5 3)]
+    [check-equal? Rs '((((pizza))) (((((pizza))))) (((pizza))))])
+
+;;; page 114
+
+(define deepM2 ; without deepR
   (lambda (n)
-    (if (member? n Ns) 
-        (find-v2 n Ns Rs) 
-        (let ((output (deep n)))
-          (set! Ns (cons n Ns))
-          (set! Rs (cons output Rs))
-          output))))
-
-(module+ test
-  (check-equal? Ns '(2 0))
-  (check-equal? Rs '(((pizza)) pizza))
-  (check-equal? (deep-M 1) '(pizza))
-  (check-equal? (deep-M 2) '((pizza)))
-  (check-equal? (deep-M 3) '(((pizza))))
-  (check-equal? (deep-M 4) '((((pizza)))))
-  (check-equal? Rs '(((((pizza)))) (((pizza))) (pizza) ((pizza)) pizza)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define Ns_3 '(3))
-(define Rs_3 '((((pizza)))))
-
-(define find-v3
-  (lambda (n Nx Rx)
-    (letrec
-        ((A (lambda (ns rs)
-              (cond
-                [(eq? n (car ns)) (car rs)]
-                [else (A (cdr ns) (cdr rs))]))))
-        (A Nx Rx))))
-
-(define deep-M3
-  (lambda (n)
-    (if (member? n Ns_3) 
-        (find-v3 n Ns_3 Rs_3) 
-        (let ((output (deep-v3 n)))
-          (set! Ns_3 (cons n Ns_3))
-          (set! Rs_3 (cons output Rs_3))
-          output))))
-
-(define deep-v3
-  (lambda (n)
-    (cond 
-      [(zero? n) 'pizza]
-      [else (cons (deep-M3 (- n 1)) '())])))
-
-(module+ test
-  (check-equal? (deep-M3 6) '((((((pizza)))))))
-  (check-equal? Ns_3 '(6 5 4 3))
-  (check-equal? (deep-M3 9) '(((((((((pizza))))))))))
-  (check-equal? Ns_3 '(9 8 7 6 5 4 3)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define find-v4
-  (lambda (n Nx Rx)
-    (letrec
-        ((A (lambda (ns rs)
-              (cond
-                [(null? ns) #f]
-                [(eq? n (car ns)) (car rs)]
-                [else (A (cdr ns) (cdr rs))]))))
-        (A Nx Rx))))
-
-(define deep-M4
-  (let ((Rs '())
-        (Ns '()))
-    (lambda (n)
-      (if (member? n Ns) 
-          (find-v4 n Ns Rs) 
-          (let ((output (deep-v4 n)))
+    (let ((gotit (find n Ns Rs)))
+      (if gotit
+          gotit
+          (let ((result (deep n)))
             (set! Ns (cons n Ns))
-            (set! Rs (cons output Rs))
-            output)))))
+            (set! Rs (cons result Rs))
+            result)))))
 
-(define deep-M5
-  (let ((Rs '())
-        (Ns '()))
+(test-case "page 114"
+    (set! Ns '())
+    (set! Rs '())
+    [check-equal? (deepM2 3) '(((pizza)))]
+    [check-equal? (deepM2 3) '(((pizza)))]
+    [check-equal? (deepM2 3) '(((pizza)))]
+    [check-equal? (deepM2 5) '(((((pizza)))))]
+    [check-equal? (deepM2 3) '(((pizza)))]
+    [check-equal? (deepM2 3) '(((pizza)))]
+    [check-equal? (deepM2 5) '(((((pizza)))))]
+    [check-equal? Ns '(5 3)]
+    [check-equal? Rs '((((((pizza))))) (((pizza))))])
+
+;;; page 115
+
+(test-case "page 115"
+    (set! Ns '())
+    (set! Rs '())
+    (set! deep (lambda (m)   ; <==== re-defining inside the testcase
+            (cond
+              [(zero? m) 'pizza]
+              [else (cons (deepM2 (sub1 m)) '())])))
+    [check-equal? (deepM2 3) '(((pizza)))]
+    [check-equal? (deepM2 5) '(((((pizza)))))]
+    [check-equal? (deepM2 3) '(((pizza)))]
+    [check-equal? Ns '(5 4 3 2 1 0)]
+    [check-equal? Rs '((((((pizza))))) ((((pizza)))) (((pizza))) ((pizza)) (pizza) pizza)])
+
+;;; page 116 -- still infringing the 16th!!
+
+(define deepM3
+  (let ((Ns '())
+        (Rs '())
+        (deep (lambda (m)
+                (if (zero? m) 
+                    'pizza 
+                    (cons (deepM3 (sub1 m)) '())))))
     (lambda (n)
-      (let ((exists (find-v4 n Ns Rs)))
-            (if (atom? exists)
-                (let ((output (deep-v4 n)))
-                  (set! Ns (cons n Ns))
-                  (set! Rs (cons output Rs))
-                  ;(print Ns)
-                  output)
-                (find-v4 n Ns Rs))))))
+      (let ((gotit (find n Ns Rs)))
+        (if gotit
+            gotit
+            (let ((result (deep n)))
+              (set! Ns (cons n Ns))
+              (set! Rs (cons result Rs))
+              result))))))
 
-(define deep-v4
-  (lambda (n)
-    (cond 
-      [(zero? n) 'pizza]
-      [else (cons (deep-M5 (- n 1)) '())])))
+(test-case "page 116"
+    [check-equal? (deepM3 3) '(((pizza)))]
+    [check-equal? (deepM3 5) '(((((pizza)))))]
+    [check-equal? (deepM3 3) '(((pizza)))])
 
-(module+ test
-  (check-equal? (deep-M5 6) '((((((pizza)))))))
-  (check-equal? (deep-M5 9) '(((((((((pizza)))))))))))
+;;; page 117 -- already included in my version
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; page 118 -- don't give a hoot, I like my version more
 
-(define length-v1
-  (lambda (l)
-    (cond
-      [(null? l) 0]
-      [else (+ 1 (length-v1 (cdr l)))])))
+;;; page 119
 
-(define length-v2
+;(define length
+;  (lambda (l)
+;    (if (null? l) 0 (add1 (length (cdr l))))))
+
+(define length
   (lambda (l)
     0))
 
-(set! length-v2
+(set! length
+  (lambda (l)
+    (if (null? l) 0 (add1 (length (cdr l))))))
+
+(test-case "page 119_a"
+  [check-equal? (length '()) 0]
+  [check-equal? (length '(1 2)) 2])
+
+; In the name of all that is sacred FIX THE 16th once and for all!!!
+
+(test-case "page 119_b"
+  (define length
+    (let ((longitud (lambda (l) 0)))
+      (set! longitud 
+            (lambda (l)
+              (if (null? l) 0 (add1 (longitud (cdr l))))))
+      longitud))
+  [check-equal? (length '()) 0]
+  [check-equal? (length '(1 2)) 2])
+
+;;; page 120 & 121 -- shit! now FIX THE 17th!!!
+
+(test-case "page 120 & 121"
+  ; given this...
+  (define L
+    (lambda (fx)
       (lambda (l)
-        (cond
-          [(null? l) 0]
-          [else (+ 1 (length-v2 (cdr l)))])))
+        (if (null? l) 0 (add1 (fx (cdr l)))))))
+  
+  ; refactor length to use L
+  (define length
+    (let ((longitud (lambda (l) 0)))
+      (set! longitud 
+            (L (lambda (x) (longitud x))))
+      longitud))
+  [check-equal? (length '()) 0]
+  [check-equal? (length '(1 2)) 2])
 
+;;; page 122 & 123
 
+(test-case "page 122 & 123"
+  (define L
+    (lambda (fx)
+      (lambda (l)
+        (if (null? l) 0 (add1 (fx (cdr l)))))))
+  
+  ;(define Y
+  ;  (lambda (L)
+  ;    (let ((h (lambda (l) '())))
+  ;    (set! h 
+  ;          (L (lambda (x) (h x))))
+  ;    h)))
+  
+  (define Y ; refactor
+    (lambda (f)
+      (letrec ((h (f (lambda (x) (h x)))))
+        h)))
+  
+  (define length (Y L))
+  [check-equal? (length '()) 0]
+  [check-equal? (length '(1 2)) 2])
+  
+; ok, it is time to face the reality of one's limitations and plunge into drunken stupor
