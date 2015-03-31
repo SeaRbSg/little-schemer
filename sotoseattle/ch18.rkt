a73588695f64b7abd70273005b643f87f01aed87
Javier Soto <sotoseattle@gmail.com>
2015-03-31 15:16:04 -0700

    finally, ch18 done and with a begining hold on all its moving parts

--- sotoseattle/ch18.rkt
+++ sotoseattle/ch18.rkt
@@ -4,15 +4,19 @@
 
 ;;; page 143
 
+(define kons cons)   ; i will redefine them later with set!
+(define kar car)
+(define kdr cdr)
+
 (define lots
   (lambda (m)
     (if (zero? m) '()
-        (cons 'egg (lots (sub1 m))))))
+        (kons 'egg (lots (sub1 m))))))
 
 (define lenkth
   (lambda (l)
     (if (null? l) 0
-        (add1 (lenkth (cdr l))))))
+        (add1 (lenkth (kdr l))))))
 
 (test-case "page 143"
   [check-equal? (lots 3)  '(egg egg egg)]
@@ -32,13 +36,13 @@
       (set! set-kounter (lambda (x) (set! N x)))
       (lambda (x y)
         (set! N (add1 N))
-        (cons x y))))
+        (kons x y))))
 
 (define add-at-end
   (lambda (l)
-    (if (null? (cdr l))
-        (konsC (car l) '(egg))
-        (konsC (car l) (add-at-end (cdr l))))))
+    (if (null? (kdr l))
+        (konsC (kar l) (kons 'egg '()))
+        (konsC (kar l) (add-at-end (kdr l))))))
 
 (test-case "page 144"
   [check-equal? (add-at-end '(1 2 3 4)) '(1 2 3 4 egg)]
@@ -47,15 +51,16 @@
 ;;; page 145
 
 (require r5rs)
+(define set-kdr set-cdr!)
 
 (define add-at-end-too
   (lambda (l)
     (letrec
         ((A (lambda (ls)
-              (if (null? (cdr ls))
-                  (set-cdr! ls (konsC 'egg '()))
-                  ;(set-cdr! ls '(egg))
-                  (A (cdr ls))))))
+              (if (null? (kdr ls))
+                  (set-kdr ls (konsC 'egg '()))
+                  ;(set-kdr ls '(egg))
+                  (A (kdr ls))))))
       (A l)
       l)))
 
@@ -67,16 +72,16 @@
 
 ;;; page 146
 
-(define kons ; given this definition ==> come up with kar and kdr
+(set! kons ; given this definition ==> come up with kar and kdr
   (lambda (head tail)
     (lambda (selector)
       (selector head tail))))
 
-(define kar
+(set! kar
   (lambda (k)
     (k (lambda (a d) a))))
 
-(define kdr
+(set! kdr
   (lambda (k)
     (k (lambda (a d) d))))
 
@@ -133,7 +138,7 @@
   (lambda (b)
      (b (lambda (s a d) d))))
 
-(define set-kdr
+(set! set-kdr
   (lambda (b x)
     ((b (lambda (s a d) s)) x)))
 
@@ -143,39 +148,34 @@
        (set-kdr b d)
        b)))
 
+(define to-list ; great idea Scott & Ryan !!
+  (lambda (p)
+    (if (null? p)
+        '()
+        (cons (kar p) (to-list (kdr p))))))
+
 (test-case "page 146 c"
-  (define hola (kons 'pepe '(luis)))
+  (define hola (kons 'pepe (kons 'luis '())))
+  (to-list hola)
+  [check-equal? (to-list hola) '(pepe luis)]
   [check-equal? (kar hola) 'pepe]
-  [check-equal? (kdr hola) '(luis)])
+  [check-equal? (kar (kdr hola)) 'luis]
+  [check-equal? (kdr (kdr hola)) '()])
 
 ;;; page 147 & 148 -- lets redefine everything
 
-(set! lots
-  (lambda (m)
-    (if (zero? m) '()
-        (kons 'egg (lots (sub1 m))))))
-
-(set! add-at-end
-  (lambda (proc)
-    (if (null? (kdr proc))
-        (kons (kar proc) (kons 'egg '()))
-        (kons (kar proc) (add-at-end (kdr proc))))))
-
-(set! add-at-end-too
-  (lambda (proc)
-    (letrec
-        ((A (lambda (p)
-              (if (null? (kdr p))
-                  (set-kdr p (kons 'egg '()))
-                  (A (kdr p))))))
-      (A proc)
-      proc)))
-
 (set-kounter 0)
 (define dozen (lots 12))
-(define bakers-dozen (add-at-end dozen))
-(define bakers-dozen-too (add-at-end-too dozen))
-(define bakers-dozen-again (add-at-end-too dozen))
+(define bakers-dozen (add-at-end (lots 12)))
+(define bakers-dozen-too (add-at-end-too (lots 12)))
+(define bakers-dozen-again (add-at-end-too (lots 12)))
+
+(test-case "basic egg baskets on procs"
+  [check-equal? (lenkth dozen)              12]
+  [check-equal? (to-list dozen) '(egg egg egg egg egg egg egg egg egg egg egg egg)]
+  [check-equal? (lenkth bakers-dozen)       13]
+  [check-equal? (lenkth bakers-dozen-too)   13]
+  [check-equal? (lenkth bakers-dozen-again) 13])
 
 ;;; page 149
 
@@ -184,8 +184,13 @@
     (cond
       [(null? l1) (null? l2)]
       [(null? l2) #f]
-      [else (and (eq? (car l1) (car l2))
-                 (eklist? (cdr l1) (cdr l2)))])))
+      [else (and (eq? (kar l1) (kar l2))
+                 (eklist? (kdr l1) (kdr l2)))])))
+
+(test-case "eklist applied to procs"
+  [check-true (eklist? (kons 'egg '()) (kons 'egg '()))]
+  [check-true (eklist? bakers-dozen bakers-dozen-too)]
+  [check-true (eklist? bakers-dozen-too bakers-dozen-again)])
 
 (define same?
   (lambda (c1 c2)
@@ -198,11 +203,58 @@
         (set-kdr c2 t2)
         v))))
 
-(kar dozen)
-(kdr dozen)
-(kdr bakers-dozen-too)
-(same? dozen bakers-dozen)
-;(kdr bakers-dozen-too)
-;(same? bakers-dozen bakers-dozen-too)
+(test-case "eklist applied to procs"
+  [check-false (same? (kons 'egg '()) (kons 'egg '()))]
+  [check-false (same? dozen bakers-dozen-too)]         ; ERRATA ?
+  [check-false (same? bakers-dozen bakers-dozen-too)]) ; DOUBT
 
-;; Oh dammit!
+;;; page 151 & 152
+
+(define last-kons
+  (lambda (proc)
+    (if (null? (kdr proc))
+        proc
+        (last-kons (kdr proc)))))
+
+(define long (lots 12))
+
+(test-case "God! the fun never ends..."
+  [check-equal? (lenkth long) 12]
+  [check-equal? (to-list long) '(egg egg egg egg egg egg egg egg egg egg egg egg)]
+  [check-equal? (lenkth (last-kons long)) 1]
+  [check-equal? (kar (last-kons long)) 'egg]
+  [check-equal? (kdr (last-kons long)) '()])
+
+(set-kdr (last-kons long) long)               ; setting the last '() to point to long (circular ref?)
+;(eklist? long long)                          ; crash!
+;(lenkth long)                                ; crash!
+;(set-kdr (last-kons long) (kdr (kdr long)))  ; crash! (kdr (kdr long)) ==> 10 eggs
+
+;;; page 153
+
+(define finite-lenkth
+  (lambda (p)
+    (let/cc infinite
+      (letrec
+          ((C (lambda (p q)
+                (cond
+                  [(same? p q) (infinite #f)]
+                  [(null? q) 0]
+                  [(null? (kdr q)) 1]
+                  [else (+ (C (sl p) (qk q)) 
+                           2)])))
+           (qk (lambda (x) (kdr (kdr x))))
+           (sl (lambda (x) (kdr x))))
+        (cond
+          [(null? p) 0]
+          [else (add1 (C p (kdr p)))])))))
+
+(test-case "finite-lenkth"
+  (set! long (lots 12))
+  (check-equal? (finite-lenkth long) 12)
+  (set-kdr (last-kons long) long)
+  (check-equal? (finite-lenkth long) #f)
+  (define mongo (kons 'pie (kons 'a (kons 'la (kons 'mode '())))))
+  [check-equal? (to-list mongo) '(pie a la mode)]
+  (set-kdr (kdr (kdr (kdr mongo))) (kdr mongo))
+  (check-equal? (finite-lenkth mongo) #f))
