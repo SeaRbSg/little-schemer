58280b2d5fc84f6cd7d0c5a7cb61fe3f52db3909
Javier Soto <sotoseattle@gmail.com>
2015-04-07 15:33:57 -0700

    ch19 done, feeling woozy

--- sotoseattle/ch19.rkt
+++ sotoseattle/ch19.rkt
@@ -65,26 +65,26 @@
 
 (define deepB_initial
   (lambda (m)
-    (if (zero? m) 
+    (if (zero? m)
         (let/cc jump
           (set! toppings jump) ; HA! the reverse function when we reach 0!!
           'pizza)
         (cons (deepB (sub1 m)) '()))))
-  
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ; this use of let/cc seems freaky at first. Revisit ch13_bis.rkt and the last approach to understand it.
 ; http://www.cs.indiana.edu/~dfried/appcont.pdf
 ; ---------------------------------------------
-; The continuation for some computation is whatever comes after it, expressed as a function of the result 
-; for that computation. The ideas is that usually we think of instruction to run in a sequential manner, 
+; The continuation for some computation is whatever comes after it, expressed as a function of the result
+; for that computation. The ideas is that usually we think of instruction to run in a sequential manner,
 ; from left to right, outside to inside (parens).
-; let/cc helps us stop in the middle of the program and look in the opposite direction: right to left, 
+; let/cc helps us stop in the middle of the program and look in the opposite direction: right to left,
 ; from the inside towards the outside, like unfolding a sock, looking from the other side of the worm-hole.
 ; the idea is that it is the same code, from a diferent point of view / reference the best way is through example.
 
 (test-case "let/cc as seen from the other side"
-  [check-equal? 
+  [check-equal?
    (+ 3
      (let/cc hop               ; <= look from here backwards and outwards and define hop!!
              (/ 4 (hop 5))))
@@ -121,7 +121,7 @@
   (set! t1 (* (pepe 2) (pepe 200)))  ; <==========  Oh No, Oh No, Oh No...
   [check-equal? t1 5])               ; <==========  ... OH MY GOD!!! << Shrieeeeek >>
 
-; When we use a continuation it forgets everything around it 
+; When we use a continuation it forgets everything around it
 ; XX COMMANDMENT == Article of Faith [Don't like this kind of articles]
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -131,9 +131,9 @@
 (set! toppings 'nothingness)
 (define deepB
   (lambda (m)
-    (if (zero? m) 
+    (if (zero? m)
         (let/cc jump
-          (set! toppings jump) 
+          (set! toppings jump)
           'pizza)
         (cons (deepB (sub1 m)) '()))))
 
@@ -158,7 +158,7 @@
 ; The explanation is obvious: if let/cc creates a wormhole, that implies a micro black hole
 ; at the exit of the black hole, (where we call (toppings whatever) all that other stuff is
 ; inside the Schwarzschild radius, by definition all that stuff falls in the black hole
-; and the only stuff to get out is whatever we sent through the wormhole. That is why it ignores 
+; and the only stuff to get out is whatever we sent through the wormhole. That is why it ignores
 ; whatever surrounds the call to (toppings and only considers the first one (obvious, wasn't it?)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -173,7 +173,7 @@
        (deep&Co (sub1 m)
                 (lambda (x)                    ; this is the new collector
                   (k (cons x '()))))])))       ;
-  
+
 (test-case "161 & 162"
   (define id (lambda (x) x))                   ; we use the identity function to get the ball rolling
   [check-equal? (deep&Co 3 id) '(((pizza)))]
@@ -186,7 +186,7 @@
        (let ()
          (set! toppings k)                     ; exactly the same, but now we remember the collector
          (k 'pizza))]
-      [else 
+      [else
        (deep&CoB (sub1 m) (lambda (x) (k (cons x '()))))])))
 
 (test-case "page 163 & 164"
@@ -194,7 +194,7 @@
   [check-equal? (toppings 'garlic) '((garlic))]              ; toppings is now the procedure 2-layers
   [check-equal? (deep&CoB 4 (lambda (x) x)) '((((pizza))))]
   [check-equal? (toppings 'garlic) '((((garlic))))]          ; toppings is now the procedure 4-layers
-  [check-equal? (cons (toppings (quote cake)) 
+  [check-equal? (cons (toppings (quote cake))
                       (toppings (quote cake))) '(((((cake)))) (((cake))))])
 
 ; the collector is a procedure, not a continuation.
@@ -222,7 +222,6 @@
   [check-false (two-in-a-row? '(1 2 3 4 3))]
   [check-true  (two-in-a-row? '(1 2 3 3 4))])
 
-
 (define leave '())
 (define walk
   (lambda (l)
@@ -249,7 +248,7 @@
 ;       - calls (leave found_atom) == (here found_atom):                   [4]
 ;             - sends the found_atom through that entrance of the wormhole [5]
 ;   - the found_atom shows up in the wormhole exit [1], and it is over!    [1]
-;       
+
 ; the idea is that there are two ways to define the exit of the wormhole:
 ; inside a function: page 167 - def leftmost -> skip but then it is only available inside leftmost
 ; outside the function: page 167 - def start-it -> here setting it to leave defined outside, 
@@ -284,7 +283,7 @@
           ((W (lambda (l)
                 (cond
                   [(null? l) '()]
-                  [(atom? (car l)) 
+                  [(atom? (car l))
                    (let ()
                      (let/cc catch_it        ; [b] for fill
                        (set! fill catch_it)
@@ -307,7 +306,7 @@
   (lambda (l)
     (cond
       [(null? l) '()]
-      [(atom? (car l)) 
+      [(atom? (car l))
        (let ()
          (let/cc catch_it
            (set! fill catch_it)
@@ -335,7 +334,7 @@
   (set! t4 (waddle '((potatong) (caramelos (chips (with))) fish)))
   [check-equal? t4 'potatong])
 
-; obvious
+; (sarcasm 'obvious!)
 
 (define get-next
   (lambda (x)
@@ -350,10 +349,115 @@
   [check-equal? t5 'chips]
   (set! t5 (fill 'f_off))      ; and again, the car of the cdr
   [check-equal? t5 'with]
-)
+  (set! t5 (fill 'f_off))      ; and again, the car of the cdr
+  [check-equal? t5 'fish]
+  (set! t5 (fill 'f_off))      ; and again, but now we are stuck! uh-ho! that aint good
+  [check-equal? t5 'fish])
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+; let's fix the bug. For comparison purposes we have:
+;
+; (define start-it
+;   (lambda (l)
+;     (let/cc here
+;       (set! leave here)
+;       (walk l))))
 
+(define get-first
+  (lambda (l)
+    (let/cc here
+      (set! leave here)
+      (waddle l)
+      (leave '()))))
 
+(module+ test
+  (define t6 (get-first '(1 (2))))
+  [check-equal? t6 '1]
+  (set! t6 (get-next 'go))
+  [check-equal? t6 '2]
+  (set! t6 (get-next 'go))
+  [check-equal? t6 '()]            ; now it works when it reaches the end
+  (set! t6 (get-next 'go))
+  [check-equal? t6 '()])           ; and keeps on working
 
+; now let's go back and redefine two-in-a-row*. For comparison purposes here what we had for lats
+;
+; (define two-in-a-row?
+;  (letrec
+;      ((W (lambda (a lat)
+;            (cond
+;              [(null? lat) #f]
+;              [else
+;               (let ((nxt (car lat)))
+;                 (or (eq? nxt a) (W nxt (cdr lat))))]))))
+;    (lambda (lat)
+;      (if (null? lat)
+;          #f
+;          (W (car lat) (cdr lat))))))
 
+(define two-in-a-row*?_v0
+  (letrec
+      ((W (lambda (a)
+            (let ((nxt (get-next 'go)))
+              (if (atom? nxt)
+                  (or (eq? nxt a) (W nxt))
+                  #f)))))
+    (lambda (l)
+      (let ((fst (get-first l)))
+        (if (atom? fst)
+            (W fst)
+            #f)))))
+
+(module+ test
+  [check-true  (two-in-a-row*?_v0 '(1 1))]
+  [check-true  (two-in-a-row*?_v0 '(1 (1)))]
+  [check-true  (two-in-a-row*?_v0 '(1 () () (1)))]
+  [check-true  (two-in-a-row*?_v0 '(1 ((1))))]
+  [check-false (two-in-a-row*?_v0 '(1 (2 (1))))])
+
+; final refactoring with everything pretty-packaged
+
+(define two-in-a-row*?
+  (letrec
+      ((T? (lambda (a)
+            (let ((nxt (get-next 0)))
+              (if (atom? nxt)
+                  (or (eq? nxt a) (T? nxt))
+                  #f))))
+       (get-next
+        (lambda (x)
+          (let/cc here-again
+            (set! leave here-again)
+            (fill 'go))))
+       (fill (lambda (x) x))
+       (waddle
+        (lambda (l)
+          (cond
+            [(null? l) '()]
+            [(atom? (car l))
+             (let ()
+               (let/cc rest
+                 (set! fill rest)
+                 (leave (car l)))
+               (waddle (cdr l)))]
+            [else
+             (let ()
+               (waddle (car l))
+               (waddle (cdr l)))])))
+       (leave (lambda (x) x)))
+    (lambda (l)
+      (let ((fst (let/cc here
+                   (set! leave here)
+                   (waddle l)
+                   (leave '()))))
+        (if (atom? fst)
+            (T? fst)
+            #f)))))
+
+(module+ test
+  [check-true  (two-in-a-row*? '(1 1))]
+  [check-true  (two-in-a-row*? '(1 (1)))]
+  [check-true  (two-in-a-row*? '(1 () () (1)))]
+  [check-true  (two-in-a-row*? '(1 ((1))))]
+  [check-false (two-in-a-row*? '(1 (2 (1))))])
