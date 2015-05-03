f67e61b7e78d8cac8fec89ec61376a110061bcc1
Ryan Davis <ryand-ruby@zenspider.com>
2015-05-02 18:17:35 -0700

    ch21 (reasoned schemer ch 1)

---
+++ zenspider/ch21.rkt
@@ -0,0 +1,270 @@
+#lang racket/base
+
+(require "lib/reasonable.rkt")
+
+(module+ test
+  (require rackunit)
+
+  (define-syntax check-run*
+    (syntax-rules (=>)
+      [(_ (vars ...) conds ... => exp) (check-equal? (run* (vars ...) conds ...)
+                                                     exp)]))
+
+  (define-syntax check-run
+    (syntax-rules (=>)
+      [(_ n (vars ...) conds ... => exp) (check-equal? (run n (vars ...) conds ...)
+                                                       exp)]))
+
+  (check-run* (q)                       ; 10
+              %u
+              => none)
+
+  (check-run* (q)                       ; 11
+              (≈ #t q)
+              => '(#t))
+
+  (check-run* (q)                       ; 12
+              %u
+              (≈ #t q)
+              => none)
+
+  (check-run* (q)                       ; 13
+              %s
+              (≈ #t q)
+              => '(#t))
+
+  (check-run* (r)                       ; 15
+              %s
+              (≈ 'corn r)
+              => '(corn))
+
+  (check-run* (r)                       ; 17
+              %u
+              (≈ 'corn r)
+              => none)
+
+  (check-run* (x)                       ; 22
+              (let ([x #f])
+                (≈ #t x))
+              => none)
+
+  (check-run* (q)                       ; 23
+              (fresh (x)
+                (≈ #t x)
+                (≈ #t q))
+              => '(#t))
+
+;;;; The Law of Fresh
+  ;;
+  ;; If x is fresh, then (≈ v x) succeeds and associates x with v.
+
+  (check-run* (q)                       ; 26
+              (fresh (x)
+                (≈ x #t)
+                (≈ #t q))
+              => '(#t))
+
+;;;; The Law of ≈
+  ;;
+  ;; (≈ v w) is the same as (≈ w v)
+
+  (check-run* (q)                       ; 28
+              %s
+              => '(_.0))
+
+  (check-run* (r)                       ; 30
+              (fresh (x y)
+                (≈ (list x y) r))
+              => '((_.0 _.1)))
+
+  (check-run* (r)                       ; 31
+              (fresh (t u)
+                (≈ (list t u) r))
+              => '((_.0 _.1)))
+
+  (check-run* (r)                       ; 33
+              (fresh (x)
+                (let ([y x])
+                  (fresh (x)
+                    (≈ (list x y x) r))))
+              => '((_.0 _.1 _.0)))
+
+  (check-run* (q)                       ; 34
+              (≈ #f q)
+              (≈ #t q)
+              => none)
+
+  (check-run* (q)                       ; 35
+              (≈ #f q)
+              (≈ #f q)
+              => '(#f))
+
+  (check-run* (q)                       ; 36
+              (let ([x q])
+                (≈ #t x))
+              => '(#t))
+
+  (check-run* (r)                       ; 37
+              (fresh (x)
+                (≈ x r))
+              => '(_.0))
+
+  (check-run* (q)                       ; 38
+              (fresh (x)
+                (≈ #t x)
+                (≈ x q))
+              => '(#t))
+
+  (check-run* (q)                       ; 39
+              (fresh (x)
+                (≈ x q)
+                (≈ #t x))
+              => '(#t))
+
+  ;; can't actually do this one, because #f isn't a lambda
+  ;; (check-run* (q)                    ; 41
+  ;;             (cond-e [#f %s]
+  ;;                     [else #f])
+  ;;             => #f)
+
+  ;; closest I can get to a working example -- the lambda is basically %u
+  (check-run* (q)                       ; 41
+              (cond-e [(lambda (_) #f) %s]
+                      [else (lambda (_) #f)])
+              => none)
+
+  (check-equal? ((cond-e [%u %s]        ; 44.1
+                         [else %u]) 'does-not-matter)
+                #f)
+
+  (check-run* (q)                       ; 44.2
+              (cond-e [%u %s]
+                      [else %u])
+              => none)
+
+  (check-equal? ((cond-e [%u %u]        ; 45.1
+                         [else %s]) 'success)
+                'success)
+
+  (check-run* (q)                       ; 45.2
+              (cond-e [%u %u]
+                      [else %s])
+              => '(_.0))
+
+  ;; raw result == (first-binding . lambda-to-continue-stream)
+  (let ([result ((cond-e [%s %s]        ; 46
+                         [else %u]) 'success)])
+    (check-equal? (car result)
+                  'success)
+
+    (check-equal? ((cdr result))
+                  #f))
+
+  (check-run* (x)                       ; 47
+              (cond-e [(≈ 'olive x) %s]
+                      [(≈ 'oil x)   %s]
+                      [else %u])
+              => '(olive oil))
+
+;;;; The Law of cond-e
+  ;;
+  ;; To get more values from cond-e, pretend that the successful
+  ;; cond-e line has failed, refreshing all variables that got an
+  ;; association from that line.
+
+  (check-run 1 (x)                      ; 49
+             (cond-e [(≈ 'olive x) %s]
+                     [(≈ 'oil x)   %s]
+                     [else %u])
+             => '(olive))
+
+  (check-run* (x)                       ; 50
+              (cond-e [(≈ 'virgin x) %u]
+                      [(≈ 'olive x)  %s]
+                      [%s            %s]
+                      [(≈ 'oil x)    %s]
+                      [else %u])
+              => '(olive _.0 oil))
+
+  (check-run 2 (x)                      ; 52
+             (cond-e [(≈ 'olive x) %s]
+                     [(≈ 'oil x)   %s]
+                     [else %u])
+             => '(olive oil))
+
+  (check-run* (r)                       ; 53
+              (fresh (x y)
+                (≈ 'split x)
+                (≈ 'pea y)
+                (≈ (list x y) r))
+              => '((split pea)))
+
+  (check-run* (r)                       ; 54
+              (fresh (x y)
+                (cond-e [(≈ 'split x) (≈ 'pea y)]
+                        [(≈ 'navy x) (≈ 'bean y)]
+                        [else %u])
+                (≈ (list x y) r))
+              => '((split pea) (navy bean)))
+
+  (check-run* (r)                       ; 55
+              (fresh (x y)
+                (cond-e [(≈ 'split x) (≈ 'pea y)]
+                        [(≈ 'navy x) (≈ 'bean y)]
+                        [else %u])
+                (≈ (list x y 'soup) r))
+              => '((split pea soup) (navy bean soup)))
+
+  (define (teacup-o x)
+    (cond-e [(≈ 'tea x) %s]
+            [(≈ 'cup x) %s]
+            [else %u]))
+
+  (check-run* (x)                       ; 56
+              (teacup-o x)
+              => '(tea cup))
+
+  (check-run* (r)                       ; 57
+              (fresh (x y)
+                (cond-e [(teacup-o x) (≈ #t y) %s]
+                        [(≈ #f x) (≈ #t y)]
+                        [else %u])
+                (≈ (list x y) r))
+              => '((tea #t)
+                   (cup #t)
+                   (#f #t)))
+
+  (check-run* (r)                       ; 58
+              (fresh (x y z)
+                (cond-e [(≈ y x) (fresh (x) (≈ z x))]
+                        [(fresh (x) (≈ y x)) (≈ z x)]
+                        [else %u])
+                (≈ (list y z) r))
+              => '((_.0 _.1)
+                   (_.0 _.1)))
+
+  (check-run* (r)                       ; 59
+              (fresh (x y z)
+                (cond-e [(≈ y x) (fresh (x) (≈ z x))]
+                        [(fresh (x) (≈ y x)) (≈ z x)]
+                        [else %u])
+                (≈ #f x)
+                (≈ (list y z) r))
+              => '((#f _.0)
+                   (_.0 #f)))
+
+  (check-run* (q)                       ; 60
+              (let ([a (≈ #t q)]
+                    [b (≈ #f q)])
+                b)
+              => '(#f))
+
+  (check-run* (q)
+              (let ([a (≈ #t q)]
+                    [b (fresh (x)
+                         (≈ x q)
+                         (≈ #f x))]
+                    [c (cond-e [(≈ #t q) %s]
+                               [else (≈ #f q)])])
+                b)
+              => '(#f)))
---
+++ zenspider/lib/reasonable.rkt
@@ -0,0 +1,286 @@
+#lang racket/base
+
+;; A goal g is a function that maps a substitution s to an ordered
+;; sequence s* of zero or more substitutions. (For clarity we notate
+;; lambda as λg when creating such a function g.) Because the sequence
+;; of substitutions may be infinite, we represent it not as a list but
+;; a stream.
+;;
+;; Streams contain either zero, one, or more substitutions. We use
+;; (mzero) to represent the empty stream of substitutions. For
+;; example, #%u maps every substitution to (mzero). If a is a
+;; substitution, then (unit a) represents the streams containing just
+;; a. For instance, #%s maps every substitution to just (unit s). The
+;; goal created by an invocation of the ≈ operator maps a substitution
+;; s to either (mzero) or to a stream containing a single (possibly
+;; extended) substitution, depending on whether that goal fails or
+;; succeeds. To represent a stream containing multiple substitutions,
+;; we use (choice a f), where a is the first substitution in the
+;; stream, and where f is a function of zero arguments. Invoking the
+;; function f produces the remainder of the stream, which may or may
+;; not be empty. (For clarity, we notate lambda as λf when creating
+;; such a function f.)
+;;
+;; When we use the variable a rather than s for substitutions, it is
+;; to emphasize that this representation of streams works for other
+;; kinds of data, as long as the datum is never #f or a pair whose cdr
+;; is a function--in other words, as long as the three cases above are
+;; never represented in overlapping ways. To discriminate among the
+;; cases we define the macro case∞.
+;;
+;; The second case is redundant in this representation: (unit a) can
+;; be represented as (choice a (λf '() #f)). We include unit, which
+;; avoids building and taking apart pairs and invoking functions
+;; because many goals never return multiple substitutions. run
+;; converts a stream of substitutions s∞ to a list of values using
+;; map∞.
+
+;; Two streams can be merged either by concatenating them using mplus
+;; (also known as stream-append) or by interleaving them using
+;; mplus-i. The only difference between the definitions mplus and
+;; mplus-i lies in the recursive case: mplus-i swaps the two streams;
+;; mplus does not.
+
+;; Given a stream s∞ and a goal g, we can feed each value in s∞ to the
+;; goal g to get a new stream, then merge all these new streams
+;; together using mplus or mplus-i. When using mplus, this operation
+;; is called monadic bind, and it is used to implement the fair
+;; conjunction all-i. The operators all and all-i are like 'and', since
+;; they are short circuiting: the false value short-circuits 'and', and
+;; any failed goal short-circuits all and all-i. Also, the let in the
+;; third clause of all-aux ensures that (all e), (all-i e), (all e
+;; #%s), and (all-i e #%s) are equivalent to e, even if the expression
+;; e has no value. The addition of the superfluous second clause
+;; allows all-aux expressions to expand to simpler code.
+
+;; To take the disjunction of goals we define cond-e, and to take the
+;; fair disjunction we define cond-i. They combine successive
+;; question-answer lines using mplus and mplus-i, respectively. Two
+;; stranger kinds of disjunction are cond-a and cond-u. When a
+;; question g0 succeeds, both cond-a and cond-u skip the remaining
+;; lines. However, cond-u chops off every substitution after the first
+;; produced by g0, whereas cond-a leaves the stream produced by g0
+;; intact.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(provide %s
+         %u
+         ≈
+         cond-e
+         fresh
+         none
+         run*
+         run)
+
+(require (only-in racket/list empty))
+
+(define none empty)
+
+(define var vector)                     ; 9:6
+
+(define var? vector?)                   ; 9:6
+
+(define empty-s '())                    ; 9:13
+
+(define (walk v s)                      ; 9:27
+  (let ((rhs cdr))
+    (cond ((var? v)
+           (cond
+             ((assq v s) => (lambda (a) (walk (rhs a) s)))
+             (else v)))
+          (else v))))
+
+(define (ext-s x v s)                   ; 9:29
+  (cons (cons x v) s))
+
+(define (unify v w s)                   ; 9:36
+  (if (eq? v w) s                       ; 9:37 optimization
+      (let ((v (walk v s))
+            (w (walk w s)))
+        (cond ((eq? v w) s)
+              ((var? v) (ext-s v w s))
+              ((var? w) (ext-s w v s))
+              ((and (pair? v) (pair? w))
+               (cond
+                 ((unify (car v) (car w) s) =>
+                  (lambda (s) (unify (cdr v) (cdr w) s)))
+                 (else #f)))
+              ((equal? v w) s)
+              (else #f)))))
+
+(define (walk* v s)                     ; 9:47
+  (let ((v (walk v s)))
+    (cond ((var? v) v)
+          ((pair? v) (cons (walk* (car v) s)
+                           (walk* (cdr v) s)))
+          (else v))))
+
+(define size-s length)
+
+(define (reify-name n)
+  (string->symbol (string-append "_" "." (number->string n))))
+
+(define (reify-s v s)                   ; 9:52
+  (let ((v (walk v s)))
+    (cond ((var?  v) (ext-s v (reify-name (size-s s)) s))
+          ((pair? v) (reify-s (cdr v) (reify-s (car v) s)))
+          (else s))))
+
+(define (reify v)
+  (walk* v (reify-s v empty-s)))
+
+(define-syntax run                      ; 9 : 6, 13, 47, 58
+  (syntax-rules ()
+    ((_ ñ (x) g ...)
+     (let ((n ñ)
+           (x (var 'x)))
+       (if (or (not n) (> n 0))
+           (map∞ n (lambda (s) (reify (walk* x s)))
+                 ((all g ...) empty-s))
+           '())))))
+
+(define-syntax run*
+  (syntax-rules ()
+    ((_ (x) g ...) (run #f (x) g ...))))
+
+(define-syntax run1
+  (syntax-rules ()
+    ((_ (x) g ...) (run 1 (x) g ...))))
+
+(define-syntax case∞
+  (syntax-rules ()
+    ((_ e zero ((â) one) ((a f) many))
+     (let ((a∞ e))
+       (cond ((not a∞) zero)
+             ((not (and (pair? a∞) (procedure? (cdr a∞))))
+              (let ((â a∞))
+                one))
+             (else (let ((a (car a∞))
+                         (f (cdr a∞)))
+                     many)))))))
+
+
+(define-syntax mzero
+  (syntax-rules ()
+    ((_) #f)))
+
+(define-syntax unit
+  (syntax-rules ()
+    ((_ a) a)))
+
+(define-syntax choice
+  (syntax-rules ()
+    ((_ a f) (cons a f))))
+
+(define (map∞ n p a∞)
+  (case∞ a∞
+         '()
+         ((a) (cons (p a) '()))
+         ((a f) (cons (p a) (cond
+                              ((not n) (map∞ n p (f)))
+                              ((> n 1) (map∞ (- n 1) p (f)))
+                              (else '()))))))
+
+(define-syntax λg (syntax-rules () ((_ a c ...) (lambda a c ...))))
+(define-syntax λf (syntax-rules () ((_ a c ...) (lambda a c ...))))
+
+(define #%s (λg (s) (unit s)))
+(define #%u (λg (s) (mzero)))
+(define %s #%s)                         ; TODO: cleanup
+(define %u #%u)                         ; TODO: cleanup
+
+
+;; (set-sharp-read-syntax! #\s (lambda (port) '#%s))
+;; (set-sharp-read-syntax! #\u (lambda (port) '#%u))
+
+(define (≈ v w)
+  (λg (s)
+      (cond
+        ((unify v w s) => #%s)
+        (else (#%u s)))))
+
+(define-syntax fresh
+  (syntax-rules ()
+    ((_ (x ...) g ...)
+     (λg (s)
+         (let ((x (var 'x)) ...)
+           ((all g ...) s))))))
+
+(define-syntax all    (syntax-rules () ((_ g ...) (all-aux bind  g ...))))
+(define-syntax all-i  (syntax-rules () ((_ g ...) (all-aux bind-i g ...))))
+
+(define-syntax all-aux
+  (syntax-rules ()
+    ((_ bnd)          #%s)
+    ((_ bnd g)        g)
+    ((_ bnd g0 g ...) (let ((g^ g0))
+                        (λg (s)
+                            (bnd (g^ s)
+                                 (λg (s) ((all-aux bnd g ...) s))))))))
+
+(define-syntax cond-e (syntax-rules () ((_ c ...) (cond-aux if-e c ...))))
+(define-syntax cond-i (syntax-rules () ((_ c ...) (cond-aux if-i c ...))))
+(define-syntax cond-a (syntax-rules () ((_ c ...) (cond-aux if-a c ...))))
+(define-syntax cond-u (syntax-rules () ((_ c ...) (cond-aux if-u c ...))))
+
+(define-syntax cond-aux
+  (syntax-rules (else)
+    ((_ ifer) #%u)
+    ((_ ifer (else g ...))     (all g ...))
+    ((_ ifer (g ...))          (all g ...))
+    ((_ ifer (g0 g ...) c ...) (ifer g0 (all g ...) (cond-aux ifer c ...)))))
+
+(define (mplus a∞ f)
+  (case∞ a∞
+         (f)
+         ((a)    (choice a f))
+         ((a f0) (choice a (λf () (mplus   (f0) f))))))
+
+(define (mplus-i a∞ f)
+  (case∞ a∞
+         (f)
+         ((a)    (choice a f))
+         ((a f0) (choice a (λf () (mplus-i (f) f0))))))
+
+(define (bind a∞ g)
+  (case∞ a∞
+         (mzero)
+         ((a)   (g a))
+         ((a f) (mplus   (g a) (λf () (bind   (f) g))))))
+
+(define (bind-i a∞ g)
+  (case∞ a∞
+         (mzero)
+         ((a)   (g a))
+         ((a f) (mplus-i (g a) (λf () (bind-i (f) g))))))
+
+(define-syntax if-e
+  (syntax-rules ()
+    ((_ g0 g1 g2)
+     (λg (s) (mplus ((all g0 g1) s) (λf () (g2 s)))))))
+
+(define-syntax if-i
+  (syntax-rules ()
+    ((_ g0 g1 g2)
+     (λg (s) (mplus-i ((all g0 g1) s) (λf () (g2 s)))))))
+
+(define-syntax if-a
+  (syntax-rules ()
+    ((_ g0 g1 g2)
+     (λg (s)
+         (let ((s∞ (g0 s)))
+           (case∞ s∞
+                  (g2 s)
+                  ((s) (g1 s))
+                  ((s f) (bind s∞ g1))))))))
+
+(define-syntax if-u
+  (syntax-rules ()
+    ((_ g0 g1 g2)
+     (λg (s)
+         (let ((s∞ (g0 s)))
+           (case∞ s∞
+                  (g2 s)
+                  ((s) (g1 s))
+                  ((s f) (g1 s))))))))
