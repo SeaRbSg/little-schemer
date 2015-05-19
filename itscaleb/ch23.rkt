b96f5cac76cd0cbae6db324c6471b10bdfaa4570
Caleb Gossler <caleb.gossler@gmail.com>
2015-05-18 22:33:51 -0700

    ch23

--- itscaleb/ch23.rkt
+++ itscaleb/ch23.rkt
@@ -8,17 +8,34 @@
     (fresh (x)
            (== `(,x ,x) s))))
 
-(define loto
-  (lambda (l)
+(define listofo
+  (lambda (predo l)
     (conde
      ((nullo l) s#)
      ((fresh (a)
              (caro l a)
-             (twinso a))
+             (predo a))
       (fresh (d)
              (cdro l d)
-             (loto d)))
+             (listofo predo d)))
      (else u#))))
+
+(define loto
+  (lambda (l)
+    (listofo twinso l)))
+
+(define membero
+  (lambda (x l)
+    (conde
+     ((nullo l) u#)
+     ((eq-caro l x) s#)
+     (else (fresh (d)
+                  (cdro l d)
+                  (membero x d))))))
+
+(define eq-caro
+  (lambda (l x)
+    (caro l x)))
 
 (check-equal? (run* (z)
                     (twinso `(,z tofu)))
@@ -32,3 +49,31 @@
 (check-equal? (run 1 (z)
                     (loto `((g g) . ,z)))
               '(()))
+
+(check-equal? (run 3 (out)
+                   (fresh (w x y z)
+                          (== `((g g) (e ,w) (,x ,y) . ,z) out)
+                          (loto out)))
+              '(((g g) (e e) (_.0 _.0))
+                ((g g) (e e) (_.0 _.0) (_.1 _.1))
+                ((g g) (e e) (_.0 _.0) (_.1 _.1) (_.2 _.2))))
+
+(check-equal? (run 1 (y)
+                   (membero y '(hummus with pita)))
+              '(hummus))
+
+(check-equal? (run* (y)
+                    (membero y '(hummus with pita)))
+              '(hummus with pita))
+
+(check-equal?  (run* (x)
+                     (membero 'e `(pasta e ,x fagioli)))
+               '(_.0 e))
+
+(check-equal? (run* (r)
+                    (fresh (x y)
+                           (membero 'e `(pasta ,x fagioli ,y))
+                           (== `(,x ,y) r)))
+              '((e _.0) (_.0 e)))
+
+
