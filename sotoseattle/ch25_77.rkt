484575b73ca1f1fed5217f7e676fdb11f3b22ee9
Javier Soto <sotoseattle@gmail.com>
2015-06-09 11:10:33 -0700

    finally ch25 #77 is solved

--- sotoseattle/ch25_77.rkt
+++ sotoseattle/ch25_77.rkt
@@ -1,9 +1,9 @@
 #lang racket
-(require "../basic_defs.rkt")
-(require "../lib/shared.rkt")
+(require "basic_defs.rkt")
+(require "lib/shared.rkt")
 (require rackunit)
 (require racket/trace)
-(require "../../lib/mk.rkt")
+(require "../lib/mk.rkt")
 
 (define appendo
   (lambda (l s out)
@@ -15,12 +15,11 @@
          (conso a res out))))))
 
 (define flatuleno
-  #| (lambda-limited 3 (s out) |#
   (lambda (s out)
     (conde
       ((conso s '() out))
       ((nullo s) (== s out))
-      ((fresh (a d aa dd)
+      ((fresh (a d aa dd y w z)
          (conso a d s)
          (flatuleno a aa)
          (flatuleno d dd)
@@ -28,9 +27,9 @@
 
 ; # 77
 [check-equal?
-  (run 1 (x)
+  (run 2 (x)
     (flatuleno x '(a b c)))
-  '((a b . c))]
+  '((a b . c)(a b c))]
 
 ; the first two condes will fails as we enter
 ; the third conde is:
@@ -59,64 +58,68 @@
      (_.0 _.1 _.2 _.3 . _.4)       ;  (_.0 _.1 _.2 _.3 _.4)
      )] ; never ends
 
-; and the (appendo _.0 _.1 '(a b c)) also can be extracted and analyzed
-[check-equal?
-  (run 4 (q)
-    (fresh (x y)
-      (appendo x y '(a b c))
-      (== `(,x ,y) q)))
- '((() (a b c))
-   ((a) (b c))
-   ((a b) (c))
-   ((a b c) ()))] ; <--- solutions taken from here ???
-;   ==> a 5th solution is never found ==> infinite-loop-entrance
-;       this infinite loop is not an infinite stream of sols like the above flatulenos
-;       it is just a black hole, once inside no solution is found, ever...more
-
-; Not all solutions of aa and dd are compatible with the appendo or first conso
-; Yet, there are more than 2. The following values work:
+; It goes DEPTH FIRST and sequentially, although it takes into consideration the existance of
+; these kind-of-lazy stream to remember how we iterate over them
+; It is sequential (flatuleno a aa) -> (flatuleno d dd) -> (appendo)
 ;
-; |   aa     |  dd     |  a        |   d       | s: (conso a d)
-; +----------+---------+-----------+-----------+----------------
-; |   (a)    | (b c)   | (a)       | (b c)     | ((a) b c)
-; |   (a)    | (b c)   | (a)       | (b . c)   | ((a) b . c)
-; |  (a b)   |  (c)    | (a b)     | c         | ((a b) c)
-; |          |  (c)    | (a . b)   | c         | ((a . b) c)
-; | (a b c)  |  ()     | (a b . c) | ()        | (a b . c)    <== 1st sol found
-; | (a b c)  |  ()     | (a b c)   | ()        | (a b c)      <== 2nd & last sol
-; |   ()     | (a b c) | ()        | (a b . c) | (() a b . c)
-; |   ()     | (a b c) | ()        | (a b c)   | (() a b c)
+; stream_aa.each do |sol_aa|
+;   strem_dd.each do |sol_dd|
+;     appendo sol_aa sol_dd '(a b c)
+;   end
+; end
+
+; So I get the first solution of (flatteno a aa) => aa == `(,a)
+;   Then take the first of (flatteno d dd) wich is also dd == `(,d)
+;   I try to appendo and fail
+;   Then I try with the SECOND sol of (flatteno d dd) => dd == d == ()
+;   Try to appendo and fail
+;   The I try the THIRD (flatteno d dd) => dd==(_0. _.1) and d==(_.0 . _.1)
+;   Try to appendo and succeed!! => First solution (a b . c) where a==(a) & d==(b . c)
+;   Try for next sol of (flatteno d dd) => dd==(_.0) ==> fail appendo (as before)
+;   Try for next sol of (flatteno d dd) => dd==(_.0 _.1 _.2) ==> fail appendo too
+;   Try for next sol of (flatteno d dd) => dd==(_.0 _.1) and d==(_.0 _.1) propper
+;   The appendo works again! ==> Second solution
+;   ...The rest of solutions of (flatenno d dd) will fail in the appendo
+;      and since there are infinte solutions for dd, it will never stop
+
+; The proof that this explanation is valid can be walked through with the following
+; modified flatulenos, where we substitute each flatuleno recursive call with the
+; solution that we want to verify
+
+(define flatuleno_sol_1
+  (lambda (s out)
+    (conde
+      ((conso s '() out))
+      ((nullo s) (== s out))
+      ((fresh (a d aa dd y w z)
+         (conso a d s)
+         ;(flatuleno a aa)
+         (== `(,a) aa)
+         ;(flatuleno d dd)
+         (== d  `(,w . ,z))        ; FIRST SOL
+         (== dd `(,w ,z))          ; FIRST SOL
+         (appendo aa dd out))))))
 
 [check-equal?
-  (run* (x)
-      (flatuleno '((a) b c)     '(a b c))
-      (flatuleno '((a) b . c)   '(a b c))
-      (flatuleno '((a b) c)     '(a b c))
-      (flatuleno '((a . b) c)   '(a b c))
-      (flatuleno '(a b . c)     '(a b c))
-      (flatuleno '(a b c)       '(a b c))
-      (flatuleno '(() a b . c)  '(a b c))
-      (flatuleno '(() a b c)    '(a b c))
-      (== #t x))
-  '(#t)]
+  (run 1 (x)
+    (flatuleno_sol_1 x '(a b c)))
+  '((a b . c))]
 
-; Trying to figure it out we could express the code substituting the function
-; calls for the streams they represent...
-;   ...
-;   fresh (a d aa dd)
-;     (conso a d x)
-;     ((_.0) () (_.0 _.1) (_.0) (_.0 _.1 _.2) (_.0 _.1)...) ; <== aa
-;     ((_.0) () (_.0 _.1) (_.0) (_.0 _.1 _.2) (_.0 _.1)...) ; <== dd
-;     ((()(a b c)) ((a) (b c)) ((a b) (c)) ((a b c)) ∞)     ; <== (aa dd)
+(define flatuleno_sol_2
+  (lambda (s out)
+    (conde
+      ((conso s '() out))
+      ((nullo s) (== s out))
+      ((fresh (a d aa dd y w z)
+         (conso a d s)
+         ;(flatuleno a aa)
+         (== `(,a) aa)
+         ;(flatuleno d dd)
+         (== d  `(,w ,z))           ; SECOND SOL
+         (== dd `(,w ,z))           ; SECOND SOL
+         (appendo aa dd out))))))
 
-; One way to look at it is from the stream of aa, I pick the first one, then I pick
-; the first of dd, and finally I try to append ==> fail => try the next dd,......
-; It would be sequential (flatuleno a aa) -> (flatuleno d dd) -> (appendo)
-; That would be total DFS and it cannot be the solution because it would never end going through dd
-;
-; Another way would be to consider the streams in parallel, a la BFS
-; I would combine an aa with all dd, and try appendo for each pair
-; But then i) other solutions would show up before (a b . c) and ii) I would not get lost in infinity
-
-; So, no idea yet...
-
+[check-equal?
+  (run 1 (x)
+    (flatuleno_sol_2 x '(a b c)))
+  '((a b c))]
