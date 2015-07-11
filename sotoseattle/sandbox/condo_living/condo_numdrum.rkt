fe405fee6233dcc5f17e4884d9585fb46c797384
Javier Soto <sotoseattle@gmail.com>
2015-07-10 19:17:37 -0700

    condo_nundrums are easier solved with rose wine

--- sotoseattle/sandbox/condo_living/condo_numdrum.rkt
+++ sotoseattle/sandbox/condo_living/condo_numdrum.rkt
@@ -5,12 +5,13 @@
 (require racket/trace)
 (require "../../../lib/mk.rkt")
 
-; 1. Adam does not live on the top floor. A => B C D E
-; 2. Bill does not live on the bottom floor. E => A B C D
-; 3. Cora does not live on either the top or the bottom floor. => B C D
+; 1. Adam does not live on the top floor.
+; 2. Bill does not live on the bottom floor.
+; 3. Cora does not live on either the top or the bottom floor.
 ; 4. Dale lives on a higher floor than does Bill.
 ; 5. Erin does not live on a floor adjacent to Cora's.
 ; 6. Cora does not live on a floor adjacent to Bill's.
+; Floors: a(top) b c d e(ground)
 
 (define tenant
   (lambda (guy l)
@@ -53,14 +54,14 @@
         [(== x1 guy2) (not_at guy1 x2)]
         [(no_adj guy1 guy2 y)]))))
 
-; (define above
-;   (lambda (top bottom floors)
-;     (fresh (x1 x2 y)
-;       (conso x1 y floors)
-;       (caro y x2)
-;       (conde
-;         [(== x2 top) (not_at bottom x1)]
-;         [(above top bottom y)]))))
+(define above
+  (lambda (top bottom floors)
+    (fresh (x1 x2 y)
+      (conso x1 y floors)
+      (caro y x2)
+      (conde
+        [(== x1 bottom) (not_ea top y)]
+        [(above top bottom y)]))))
 
 (run* (brix)
   (fresh (a b c d e)
@@ -72,7 +73,7 @@
     (not_at 'bill e)                     ; Bill is not at ground floor
     (not_ea 'cora (list a e))            ; Cora is neither at top nor ground
 
-    ; (above 'dale 'bill brix)             ; Dale lives above Bill
+    (above 'dale 'bill brix)             ; Dale lives above Bill
 
     (no_adj 'erin 'cora brix)            ; Erin and Cora don't live in adj floors
     (no_adj 'cora 'bill brix)            ; Cora and Bill don't live in adj floors
