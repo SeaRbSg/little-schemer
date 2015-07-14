f1d11840c7b942863cf7b7e170cf2375500a9bb5
Javier Soto <sotoseattle@gmail.com>
2015-07-14 10:02:35 -0700

    leverage scheme to pick sol with 4 Queens

--- sotoseattle/sandbox/queens/4Queens.rkt
+++ sotoseattle/sandbox/queens/4Queens.rkt
@@ -3,80 +3,84 @@
 (require "../../../lib/mk.rkt")
 (require rackunit)
 
-(define (nono l x)
+(define (nono l)
   (conde
     [(nullo l)]
     [(fresh (a d)
        (conso a d l)
-       (ifa (== a x) u# (nono d x)))]))
+       (ifa (== a 'Q) u# (nono d)))]))
 
 (define at_most_once
-  (lambda (l x)
+  (lambda (l)
     (fresh (a d)
       (conso a d l)
       (conda
         [(nullo d) s#]
-        [(== a x) (nono d x)]
-        [else (at_most_once d x)]))))
+        [(== a 'Q) (nono d)]
+        [else (at_most_once d)]))))
 
 (define at_most_once_each
-  (lambda (listas thingy)
+  (lambda (listas)
     (conde
       [(nullo listas)]
       [(fresh (a d)
          (conso a d listas)
-         (at_most_once a thingy)
-         (at_most_once_each d thingy))])))
+         (at_most_once a)
+         (at_most_once_each d))])))
 
 (define initialize
-  (lambda (board thingy)
+  (lambda (board)
     (conde
       [(nullo board)]
       [(fresh (a d)
          (conso a d board)
-         (conde [(== a thingy)] [(== a '_)])
-         (initialize d thingy))])))
+         (conde [(== a 'Q)] [(== a '_)])
+         (initialize d))])))
 
-(run* (board)
-  (fresh (Q a1 a2 a3 a4 b1 b2 b3 b4 c1 c2 c3 c4 d1 d2 d3 d4)
-    (== Q 'Q)
-    (== board (list a1 a2 a3 a4
-                    b1 b2 b3 b4
-                    c1 c2 c3 c4
-                    d1 d2 d3 d4))
-    (initialize board Q)
-    (fresh (→1 →2 →3 →4
-               ↓1 ↓2 ↓3 ↓4
-               ⇗1 ⇗2 ⇗3 ⇗4 ⇗5
-               ⇘1 ⇘2 ⇘3 ⇘4 ⇘5)
+(define count
+  (lambda (l)
+    (cond
+      [(null? l) 0]
+      [(equal? (car l) 'Q) (+  1 (count (cdr l)))]
+      [else (count (cdr l))])))
 
-      (== →1 (list a1 a2 a3 a4))
-      (== →2 (list b1 b2 b3 b4))
-      (== →3 (list c1 c2 c3 c4))
-      (== →4 (list d1 d2 d3 d4))
+(define solution_4Q
+  (lambda (l n)
+    (cond
+      [(null? l) #f]
+      [(eq? (count (car l)) n) (print (car l))]
+      [(solution_4Q (cdr l) n)])))
 
-      (== ↓1 (list a1 b1 c1 d1))
-      (== ↓2 (list a2 b2 c2 d2))
-      (== ↓3 (list a3 b3 c3 d3))
-      (== ↓4 (list a4 b4 c4 d4))
-
-      (== ⇗1  (list b1 a2))
-      (== ⇗2  (list c1 b2 a3))
-      (== ⇗3  (list d1 c2 b3 a4))
-      (== ⇗4  (list d2 c3 b4))
-      (== ⇗5  (list d3 c4))
-
-      (== ⇘1  (list a3 b4))
-      (== ⇘2  (list a2 b3 c4))
-      (== ⇘3  (list a1 b2 c3 d4))
-      (== ⇘4  (list b1 c2 d3))
-      (== ⇘5  (list c1 d2))
-
-      (at_most_once_each (list →1 →2 →3 →4
-                               ↓1 ↓2 ↓3 ↓4
-                               ⇗1 ⇗2 ⇗3 ⇗4 ⇗5
-                               ⇘1 ⇘2 ⇘3 ⇘4 ⇘5) Q)
-      )))
+(let
+  ((4x4 (run* (board)
+          (fresh (a1 a2 a3 a4 b1 b2 b3 b4 c1 c2 c3 c4 d1 d2 d3 d4)
+            (== board (list a1 a2 a3 a4
+                            b1 b2 b3 b4
+                            c1 c2 c3 c4
+                            d1 d2 d3 d4))
+            (initialize board)
+            (fresh (→1 →2 →3 →4 ↓1 ↓2 ↓3 ↓4 ⇗1 ⇗2 ⇗3 ⇗4 ⇗5 ⇘1 ⇘2 ⇘3 ⇘4 ⇘5)
+              (== →1 (list a1 a2 a3 a4))
+              (== →2 (list b1 b2 b3 b4))
+              (== →3 (list c1 c2 c3 c4))
+              (== →4 (list d1 d2 d3 d4))
+              (== ↓1 (list a1 b1 c1 d1))
+              (== ↓2 (list a2 b2 c2 d2))
+              (== ↓3 (list a3 b3 c3 d3))
+              (== ↓4 (list a4 b4 c4 d4))
+              (== ⇗1  (list b1 a2))
+              (== ⇗2  (list c1 b2 a3))
+              (== ⇗3  (list d1 c2 b3 a4))
+              (== ⇗4  (list d2 c3 b4))
+              (== ⇗5  (list d3 c4))
+              (== ⇘1  (list a3 b4))
+              (== ⇘2  (list a2 b3 c4))
+              (== ⇘3  (list a1 b2 c3 d4))
+              (== ⇘4  (list b1 c2 d3))
+              (== ⇘5  (list c1 d2))
+              (at_most_once_each (list →1 →2 →3 →4 ↓1 ↓2 ↓3 ↓4 ⇗1 ⇗2 ⇗3 ⇗4 ⇗5 ⇘1 ⇘2 ⇘3 ⇘4 ⇘5)))))
+    ))
+  (solution_4Q 4x4 4))
 
 ; includes the solution _ Q _ _  with most queens in various rotations
 ;                       _ _ _ Q
